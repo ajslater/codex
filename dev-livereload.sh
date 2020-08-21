c232ddc4fca46de432e606b68d548b0704d8c8c7
AJ Slater <aj@slater.net>
2020-08-21 13:10:10 -0700

    livereload server waits for django to start

--- dev-livereload.sh
+++ dev-livereload.sh
@@ -2,4 +2,10 @@
 # Run the django livereload app to trigger reloading the frontennd when the
 #  api restarts
 cd "$(dirname "$0")" || exit 1
+printf "Waiting for dajngo server to start..."
+until lsof -i -P -n | grep -q '9810.*LISTEN'; do
+    sleep 1
+    printf "."
+done
+printf "\n"
 DEV=1 ./pm livereload
