4757e632651db3be02362531008b95e3c01f715a
AJ Slater <aj@slater.net>
2020-08-28 10:17:41 -0700

    fix sed for bsd

--- codex-version.sh
+++ codex-version.sh
@@ -2,7 +2,15 @@
 # Set version in all three places: Frontend, API, & Docker Env.
 set -euo pipefail
 VERSION="$1"
-sed -i -e "s/PKG_VERSION=.*/PKG_VERSION=v$VERSION/" ./docker-env 
+
+# BSD sed behaves differently
+if [ "$(uname)" = "Darwin" ]; then
+    sedi=('/usr/bin/sed' '-i' '')
+else
+    sedi=('sed' '-i')
+fi
+
+"${sedi[@]}" "s/PKG_VERSION=.*/PKG_VERSION=v$VERSION/" docker-env
 poetry version "$VERSION"
 cd frontend
 npm version --allow-same-version "$VERSION"
