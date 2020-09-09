85e3f2e6cf1f9edd5fe445ffc7193bd3b5a5fbf4
AJ Slater <aj@slater.net>
2020-09-08 19:11:03 -0700

    fix push script

--- ci/tag-and-push.sh
+++ ci/tag-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 source ./docker-env
-VERSION=${BASE_VERSION}_codex-${PKG_VERSION}
+VERSION=${RUNNABLE_BASE_VERSION}_codex-${PKG_VERSION}
 TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
 docker tag "$IMAGE" "$IMAGE:$TAG"
 docker tag "$IMAGE" "$IMAGE:latest"
