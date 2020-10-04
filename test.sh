1e40cca5a71b1f739159722052e29cb8f345ecae
AJ Slater <aj@slater.net>
2020-10-03 18:43:36 -0700

    remove mypy and vulture. i don't use them.

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ poetry run pytest
 # pytest-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
 bash -c "cd frontend && npm run test:unit"
 poetry run coverage erase || true
-poetry run vulture --exclude frontend . || true
+#poetry run vulture --exclude frontend . || true
 poetry run radon mi -nc . || true
