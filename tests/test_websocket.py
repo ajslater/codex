9c3568f6e00ebe162e9b184fe2043c2cc7e91a83
AJ Slater <aj@slater.net>
2020-10-08 16:25:11 -0700

    fix websocket dumb test

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -1,8 +1,22 @@
 """Test websockets."""
-from codex.websocket_server import get_send_msg
+from codex.websocket_server import send_msg
 
 
-def test_get_send_msg():
+async def send(self, msg):
+    """Send dummy message."""
+    global KEYS
+    print(self.key, msg)
+    KEYS.add(self.key, self.msg)
+
+
+KEYS = set()
+COMPARE_KEYS = set(["hello"])
+CONNS = set([send, send])
+
+
+async def test_get_send_msg():
     """Test the get_send_msg method."""
-    msg = get_send_msg("hello")
-    assert msg == {"type": "websocket.send", "text": "hello"}
+    global CONNS, KEYS
+    KEYS = set()
+    await send_msg(CONNS, "hello")
+    assert KEYS == COMPARE_KEYS
