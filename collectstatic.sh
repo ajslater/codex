c820ea15d8de240305b1f7dd2f8556a0386f2dd4
AJ Slater <aj@slater.net>
2020-10-14 16:18:29 -0700

    error exit on collect static just incase

--- collectstatic.sh
+++ collectstatic.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # Run the django collectstatic command to collect static files from all
 # locations specified in settings.STATIC_DIRS and place them in
 # settings.STATIC_ROOT for production builds.
+set -euo pipefail
 STATIC_PROD=$(poetry run python -c 'from codex.settings.settings import STATIC_ROOT; print(STATIC_ROOT)')
 STATIC_CONFIG=$(poetry run python -c 'from codex.settings.settings import CONFIG_STATIC; print(CONFIG_STATIC)')
 rm -rf "$STATIC_PROD"
