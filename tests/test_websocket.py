636d04f08f0e15877cda572c72d65ed4a0dee6d1
AJ Slater <aj@slater.net>
2020-11-17 14:16:25 -0800

    fix websocket test with pytest.asyncio

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -1,22 +1,35 @@
 """Test websockets."""
+import pytest
+
 from codex.websocket_server import send_msg
 
 
-async def send(self, msg):
-    """Send dummy message."""
-    global KEYS
-    print(self.key, msg)
-    KEYS.add(self.key, self.msg)
-
+pytestmark = pytest.mark.asyncio
 
 KEYS = set()
-COMPARE_KEYS = set(["hello"])
-CONNS = set([send, send])
 
 
-async def test_get_send_msg():
-    """Test the get_send_msg method."""
-    global CONNS, KEYS
-    KEYS = set()
-    await send_msg(CONNS, "hello")
-    assert KEYS == COMPARE_KEYS
+class Sender:
+    """Dummy asycnio sender."""
+
+    async def send(self, msg):
+        """Send dummy message."""
+        global KEYS
+        print(msg)
+        KEYS.add(str(msg))
+
+
+class TestWebsockets:
+    """Test websockets."""
+
+    HELLO_MSG = "{'text': 'hello', 'type': 'websocket.send'}"
+    COMPARE_KEYS = set([HELLO_MSG])
+
+    async def test_get_send_msg(self):
+        """Test the get_send_msg method."""
+        global KEYS
+        sender = Sender()
+        conns = set([sender.send, sender.send])
+        KEYS = set()
+        await send_msg(conns, "hello")
+        assert KEYS == self.COMPARE_KEYS
