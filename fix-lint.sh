1f485ca844abd36e011a8c08458a5ec6d6012380
AJ Slater <aj@slater.net>
2020-11-18 16:56:56 -0800

    add eslint to lint & fix

--- fix-lint.sh
+++ fix-lint.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Fix as much as many linting issues as we can
+set -euxo pipefail
 poetry run isort --color .
 poetry run black .
+bash -c "cd frontend && eslint --fix ."
 prettier --write .
