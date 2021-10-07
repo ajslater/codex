94388ecefeda223b114d4b357b9be5fae7f83508
AJ Slater <aj@slater.net>
2021-10-06 19:14:08 -0700

    fix jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,6 @@
-export default {
+// eslint-disable-next-line unicorn/prefer-module
+const {defaults} = require('jest-config');
+module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'vue'],
 };
