22c071f945add53c4f7ac7b0b319fc967fd30342
AJ Slater <aj@slater.net>
2021-10-09 14:15:27 -0700

    fix build-dist for macos

--- build-dist.sh
+++ build-dist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Buld script for producind a codex python package
+# Buld script for producing a codex python package
 set -euxo pipefail
 cd "$(dirname "$(readlink "$0")")"
 
@@ -12,9 +12,14 @@ echo "*** collect static resources into static root ***"
 cd ..
 ./collectstatic.sh
 
-
 echo "*** build and package application ***"
 # XXX poetry auto-excludes anything in gitignore. Dirty hack around that.
-sed -i "s/.*static_root.*//" .gitignore
+# XXX also a dirty hack around macOS BSD sed
+SED="sed"
+GSED="$(which gsed)"
+if [ -n "$GSED" ]; then
+	SED="$GSED"
+fi
+"$SED" -i "s/.*static_root.*//" .gitignore
 poetry build
 git checkout .gitignore # XXX so i can run this locally
