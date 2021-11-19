07bce268193125943aaa86427ee8576489fcbca1
AJ Slater <aj@slater.net>
2021-11-19 13:24:50 -0800

    count comics updated and more log comics per second

--- codex/librarian/db/updaterd.py
+++ codex/librarian/db/updaterd.py
@@ -1,5 +1,9 @@
 """Bulk import and move comics and folders."""
+import time
+
 from logging import getLogger
+
+from django.db.models.functions import Now
 
 from codex.librarian.db.aggregate_metadata import get_aggregate_metadata
 from codex.librarian.db.cleanup import cleanup_database
@@ -44,16 +48,19 @@ def _batch_modified_and_created(library, modified_paths, created_paths) -> bool:
 
     changed = _bulk_create_comic_relations(library, fks)
 
-    changed |= bulk_import_comics(
+    imported_count = bulk_import_comics(
         library, created_paths, modified_paths, mds, m2m_mds, fis
     )
-    return changed
+    changed |= imported_count > 0
+    return changed, imported_count
 
 
 def apply(task):
     """Bulk import comics."""
+    start_time = time.time()
     library = Library.objects.get(pk=task.library_id)
     LOG.verbose(f"Updating Library {library.path}...")  # type: ignore
+    library = Library.objects.get(pk=task.library_id)
     library.update_in_progress = True
     library.save()
     LIBRARIAN_QUEUE.put(AdminNotifierTask("SCAN_LIBRARY"))
@@ -62,15 +69,17 @@ def apply(task):
     changed |= bulk_folders_moved(library, task.dirs_moved)
     changed |= bulk_comics_moved(library, task.files_moved)
     changed |= bulk_folders_modified(library, task.dirs_modified)
-    changed |= _batch_modified_and_created(
+    changed_comics, imported_count = _batch_modified_and_created(
         library, task.files_modified, task.files_created
     )
+    changed |= changed_comics
     changed |= bulk_folders_deleted(library, task.dirs_deleted)
     changed |= bulk_comics_deleted(library, task.files_deleted)
     changed |= cleanup_database(library)
 
-    library.update_in_progress = False
-    library.save()
+    Library.objects.filter(pk=task.library_id).update(
+        update_in_progress=False, updated_at=Now()
+    )
     if FailedImport.objects.all().exists():
         text = "FAILED_IMPORTS"
     else:
@@ -78,6 +87,12 @@ def apply(task):
     LIBRARIAN_QUEUE.put(AdminNotifierTask(text))
     if changed:
         LIBRARIAN_QUEUE.put(BroadcastNotifierTask("LIBRARY_CHANGED"))
+        elapsed_time = time.time() - start_time
+        LOG.info(f"Updated database in {int(elapsed_time)} seconds.")
+        LOG.verbose(f"Imported {imported_count} comics.")
+        if imported_count:
+            cps = int(imported_count / elapsed_time)
+            LOG.verbose(f"Took {cps} comics per second.")
 
 
 class Updater(QueuedThread):
