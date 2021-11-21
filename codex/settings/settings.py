7ee376c912d92c5b1f66d45ce55c4e41e4f4d477
AJ Slater <aj@slater.net>
2021-11-20 16:45:02 -0800

    use local time for logging

--- codex/settings/settings.py
+++ codex/settings/settings.py
@@ -147,9 +147,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
+# https://docs.djangoproject.com/en/dev/topics/i18n/
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
