3e25ac0b6765276d2782445c848f9cd3977c6901
AJ Slater <aj@slater.net>
2021-12-04 16:25:33 -0800

    fix logging for webpack serializers loader

--- codex/serializers/webpack.py
+++ codex/serializers/webpack.py
@@ -39,8 +39,8 @@ if DEBUG:
     WEBPACK_MODULE_RE_TEMPLATES[DEV_JS_ROOT] = "^{name}.js$"
 
 
-WEBSOCKETS_MODULE_NAME = "websocketMessages"
 BROWSER_CHOICES_MODULE_NAME = "browserChoices"
+WEBSOCKETS_MODULE_NAME = "websocketMessages"
 WEBPACK_MODULE_NAMES = (
     BROWSER_CHOICES_MODULE_NAME,
     "readerChoices",
@@ -86,7 +86,7 @@ WEBSOCKET_MESSAGES = {}
 VUETIFY_NULL_CODE = -1
 
 
-def find_filename_regex(js_root, module_name):
+def _find_filename_regex(js_root, module_name):
     """Find a filename in a dir that matches the regex."""
     if not js_root.is_dir():
         LOG.warn(f"Not a directory: {js_root}")
@@ -101,7 +101,7 @@ def find_filename_regex(js_root, module_name):
     raise FileNotFoundError(f"Could not find {js_root} {module_name}")
 
 
-def extract_json(js_root, webpack_module_js):
+def _extract_json(js_root, webpack_module_js):
     """Use different args to extract the json depending on origin."""
     args = EXTRACT_JSON_ARGS[js_root]
     header_index = webpack_module_js.find(args["head"])
@@ -112,17 +112,17 @@ def extract_json(js_root, webpack_module_js):
     return json_str
 
 
-def parse_wepack_module(module_name):
+def _parse_wepack_module(module_name):
     """Extract the JSON core from a webpack module and parse it."""
-    data_dict = fn = None
+    data_dict = None
     for js_root in JS_ROOTS:
         try:
-            path = find_filename_regex(js_root, module_name)
+            path = _find_filename_regex(js_root, module_name)
             if path:
                 with path.open("r") as webpack_module_file, mmap.mmap(
                     webpack_module_file.fileno(), 0, access=mmap.ACCESS_READ
                 ) as webpack_module_js:
-                    json_str = extract_json(js_root, webpack_module_js)
+                    json_str = _extract_json(js_root, webpack_module_js)
                     data_dict = json.loads(json_str)
                     break
         except Exception as exc:
@@ -130,10 +130,10 @@ def parse_wepack_module(module_name):
     if not data_dict:
         LOG.error(f"Could not extract values from {module_name}")
 
-    return data_dict, fn
+    return data_dict
 
 
-def build_show_defaults(settings_group_list):
+def _build_show_defaults(settings_group_list):
     """Parse the show defaults."""
     show = {}
     for choice in settings_group_list:
@@ -142,12 +142,12 @@ def build_show_defaults(settings_group_list):
     return show
 
 
-def build_choices_and_defaults(data_dict):
+def _build_choices_and_defaults(data_dict):
     """Transform the vuetify choice formatted data to key:value dicts."""
     global DEFAULTS, VUETIFY_NULL_CODE, CHOICES
     for vuetify_key, vuetify_list in data_dict.items():
         if vuetify_key == "settingsGroup":
-            DEFAULTS["show"] = build_show_defaults(vuetify_list)
+            DEFAULTS["show"] = _build_show_defaults(vuetify_list)
             continue
         if vuetify_key == "vuetifyNullCode":
             VUETIFY_NULL_CODE = vuetify_list
@@ -161,11 +161,11 @@ def build_choices_and_defaults(data_dict):
         CHOICES[vuetify_key] = choices
 
 
-def load_from_webpack_modules():
+def _load_from_webpack_modules():
     """Load values from the vuetify formatted json into python dicts."""
     global WEBPACK_MODULE_NAMES, WEBSOCKETS_MODULE_NAME, WEBSOCKET_MESSAGES
     for module_name in WEBPACK_MODULE_NAMES:
-        data_dict, fn = parse_wepack_module(module_name)
+        data_dict = _parse_wepack_module(module_name)
         if not data_dict:
             return
         if module_name == WEBSOCKETS_MODULE_NAME:
@@ -173,8 +173,8 @@ def load_from_webpack_modules():
         else:
             if module_name == BROWSER_CHOICES_MODULE_NAME:
                 del data_dict["groupNames"]
-            build_choices_and_defaults(data_dict)
-        LOG.debug(f"Parsed {fn}")
+            _build_choices_and_defaults(data_dict)
+        LOG.debug(f"Parsed {module_name}")
 
 
-load_from_webpack_modules()
+_load_from_webpack_modules()
