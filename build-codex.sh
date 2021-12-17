6ee79763da9a14c2e8f4d91e5ae8997bd6960f0c
AJ Slater <aj@slater.net>
2021-12-17 10:45:30 -0800

    fix tagging on build scripts

--- build-codex.sh
+++ build-codex.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Build a codex docker image suitable for running from Dockerfile
-# Building it takes so long that login credentials time out.
-# So cache it in the buildx buildcache and push it later.
-set -xeuo pipefail
+set -euxo pipefail
 
 # Load .env variables but with a mechanism to override PLATFORMS
 source .env
@@ -10,27 +8,36 @@ if [ -z "${PLATFORMS:-}" ]; then
     source .env.platforms
 fi
 
-export DOCKER_CLI_EXPERIMENTAL=enabled
-export DOCKER_BUILDKIT=1
-
 # Different behavior for multiple vs single PLATFORMS
-if [[ $PLATFORMS =~ "," ]]; then
-    # more than one platform
-    LOAD_OR_PUSH="--push"
+if [ -n "${1:-}" ]; then
+    CMD=$1
 else
-    # only one platform loading into docker works
-    LOAD_OR_PUSH="--load"
+    if [[ ${PLATFORMS:-} =~ "," ]]; then
+        # more than one platform
+        CMD="--push"
+    else
+        # only one platform loading into docker works
+        CMD="--load"
+    fi
 fi
 
 WHEELS_VERSION=$(md5sum poetry.lock | awk '{print $1}')
 CODEX_WHEEL=codex-${PKG_VERSION}-py3-none-any.whl
+export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_BUILDKIT=1
 export CODEX_WHEEL
 export PLATFORMS
 export RUNNABLE_BASE_VERSION
 export WHEELS_VERSION
+if [ -n "${PLATFORMS:-}" ]; then
+    PLATFORM_ARG=(--set "*.platform=$PLATFORMS")
+else
+    PLATFORM_ARG=()
+fi
 # Build and cache
+# shellcheck disable=2068
 docker buildx bake codex \
-    --set "codex.platform=$PLATFORMS" \
-    --set "codex.tags=$REPO:${PKG_VERSION}" \
-    --set "codex.tags=$REPO:latest" \
-    ${LOAD_OR_PUSH:-}
+    ${PLATFORM_ARG[@]:-} \
+    --set "*.tags=$REPO:${PKG_VERSION}" \
+    --set "*.tags=$REPO:latest" \
+    ${CMD:-}
