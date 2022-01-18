ea042d7cee7890c7510a3c295b9355790a9d5579
AJ Slater <aj@slater.net>
2022-01-18 01:39:57 -0800

    safely remove static_root files from a container

--- collectstatic.sh
+++ collectstatic.sh
@@ -5,6 +5,6 @@
 set -euo pipefail
 STATIC_PROD=$(poetry run python -c 'from codex.settings.settings import STATIC_ROOT; print(STATIC_ROOT)')
 STATIC_CONFIG=$(poetry run python -c 'from codex.settings.settings import CONFIG_STATIC; print(CONFIG_STATIC)')
-rm -rf "$STATIC_PROD"
-mkdir -p "$STATIC_PROD" "$STATIC_CONFIG"
+rm -rf "${STATIC_PROD:?}"/*
+mkdir -p "${STATIC_PROD:?}" "${STATIC_CONFIG:?}"
 BUILD=1 ./pm collectstatic --clear --ignore covers --no-input
