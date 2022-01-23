7bf964579787c3db315291170715c203ba510c6a
AJ Slater <aj@slater.net>
2022-01-22 22:43:07 -0800

    fix shellcheck deps delimiter

--- docker-version-codex-builder.sh
+++ docker-version-codex-builder.sh
@@ -2,7 +2,8 @@
 # Compute the version tag for ajslater/codex-builder
 set -euo pipefail
 CODEX_BASE_VERSION=$(./docker-version-codex-base.sh)
-IFS=" " read -r -a SHELLCHECK_DEPS <<<"$(find vendor/shellcheck -type f \( ! -name "*~" \))"
+# shellcheck disable=SC2046
+read -ra SHELLCHECK_DEPS <<<$(find vendor/shellcheck -type f \( ! -name "*~" \))
 DEPS=(
     "$0"
     .dockerignore
