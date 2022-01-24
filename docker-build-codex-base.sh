5de6195e37e3e9b2218b05059552fad8d658bb8c
AJ Slater <aj@slater.net>
2022-01-24 00:21:26 -0800

    try with CODEX_BUILDER_VERSION actually set

--- docker-build-codex-base.sh
+++ docker-build-codex-base.sh
@@ -25,6 +25,8 @@ export DOCKER_BUILDKIT=1
 export PLATFORMS
 export PYTHON_ALPINE_VERSION
 export CODEX_BASE_VERSION
+CODEX_BUILDER_VERSION=$(./docker-version-codex-base.sh)
+export CODEX_BUILDER_VERSION
 if [ -n "${PLATFORMS:-}" ]; then
     PLATFORM_ARG=(--set "*.platform=$PLATFORMS")
 else
