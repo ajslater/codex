b5f5841c6d5709f1e032c7e753cfb202df821d18
AJ Slater <aj@slater.net>
2022-01-24 13:14:59 -0800

    fix docker-compose-exit script

--- docker/docker-compose-exit.sh
+++ docker/docker-compose-exit.sh
@@ -2,4 +2,5 @@
 set -eu
 export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_BUILDKIT=1
-docker compose up --exit-code-from "$1" "$1"
+# docker compose without the dash doesn't have the exit-code-from param
+docker-compose up --exit-code-from "$1" "$1"
