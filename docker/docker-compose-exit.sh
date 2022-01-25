d89adb2efd1e1b9792628eb98fc3fa0ae615e8f4
AJ Slater <aj@slater.net>
2022-01-24 23:04:29 -0800

    fix docker-compose

--- docker/docker-compose-exit.sh
+++ docker/docker-compose-exit.sh
@@ -8,6 +8,7 @@ CODEX_BUILDER_BASE_VERSION=$(./docker-version-codex-base.sh)
 export CODEX_BUILDER_BASE_VERSION
 CODEX_DIST_BUILDER_VERSION=$(./docker-version-codex-dist-builder.sh)
 export CODEX_DIST_BUILDER_VERSION
-docker compose pull "$1"
+# some circleci builders don't have docker compose without a dash
+docker-compose pull "$1"
 # docker compose without the dash doesn't have the exit-code-from param
 docker-compose up --exit-code-from "$1" "$1"
