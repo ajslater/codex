3c6aee9e5dc707be88d2959f1adfa92d3ba9af0f
AJ Slater <aj@slater.net>
2022-01-24 22:55:02 -0800

    maybe more reliable pull

--- docker/docker-compose-exit.sh
+++ docker/docker-compose-exit.sh
@@ -8,5 +8,6 @@ CODEX_BUILDER_BASE_VERSION=$(./docker-version-codex-base.sh)
 export CODEX_BUILDER_BASE_VERSION
 CODEX_DIST_BUILDER_VERSION=$(./docker-version-codex-dist-builder.sh)
 export CODEX_DIST_BUILDER_VERSION
+docker compose pull "$1"
 # docker compose without the dash doesn't have the exit-code-from param
 docker-compose up --exit-code-from "$1" "$1"
