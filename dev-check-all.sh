66a97a6ec1bc90c43d7379dce85300bd32a7dbe0
AJ Slater <aj@slater.net>
2022-01-26 15:26:56 -0800

    don't do fixes in the check script

--- dev-check-all.sh
+++ dev-check-all.sh
@@ -2,8 +2,6 @@
 # Run all fixes and then all tests & lints.
 # Good to do before commits
 set -euo pipefail
-./fix-lint-frontend.sh
-./fix-lint-backend.sh
 ./lint-frontend.sh
 ./test-frontend.sh
 ./build-frontend.sh
