e85eb9b8b71aa9b9e535bb917ebda0d7098a8534
AJ Slater <aj@slater.net>
2022-01-26 17:35:46 -0800

    clean up docker init scripts

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -4,15 +4,16 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_BUILDKIT=1
 # login to docker using environment variables
 echo "$DOCKER_PASS" | docker login --username="$DOCKER_USER" --password-stdin
-if [[ ${PLATFORMS:-} == linux/armhf ]]; then
-    PLATFORMS=arm
-elif [[ $(uname -m) == "x86_64" ]]; then
-    PLATFORMS=""
-elif [[ $(uname -m) == "aarch64" ]]; then
-    PLATFORMS=aarch64
+# install emulator binaries if i need to
+EMULATORS=
+if [[ ${PLATFORMS:-} == "linux/armhf" ]]; then
+    # this is the only arch i need to cross compile on circleci
+    EMULATORS=arm
+elif [[ "$(uname -m)" == "aarch64" ]]; then
+    EMULATORS=aarch64
 fi
-if [[ -n ${PLATFORMS:-} ]]; then
-    docker run --rm --privileged tonistiigi/binfmt:latest --install "$PLATFORMS"
+if [[ -n ${EMULATORS:-} ]]; then
+    docker run --rm --privileged tonistiigi/binfmt:latest --install "$EMULATORS"
 fi
 # buildx requires creating a builder on a fresh system
 docker buildx create --use
