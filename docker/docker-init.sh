3fa2671be31e4f754f3582e0409709a0e72cc085
AJ Slater <aj@slater.net>
2022-01-26 16:50:51 -0800

    handle no PLATFORMS, switch to large amd64 executor

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -4,10 +4,10 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_BUILDKIT=1
 # login to docker using environment variables
 echo "$DOCKER_PASS" | docker login --username="$DOCKER_USER" --password-stdin
-if [[ $PLATFORMS == linux/armhf ]]; then
+if [[ ${PLATFORMS:-} == linux/armhf ]]; then
     PLATFORMS=arm
 elif [[ $(uname -m) == "x86_64" ]]; then
-    PLATFORMS=
+    PLATFORMS=""
 elif [[ $(uname -m) == "aarch64" ]]; then
     PLATFORMS=aarch64
 fi
