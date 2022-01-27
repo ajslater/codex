da4541fe2bbb286728eb3f8f099f3cf471ddc29e
AJ Slater <aj@slater.net>
2022-01-26 22:42:21 -0800

    fix project name

--- docker-wheels-restore.sh
+++ docker-wheels-restore.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # copy codex wheels to host cache
 set -exuo pipefail
-docker-compose pull codex-wheels
-docker-compose create --no-build codex-wheels
-docker container cp --follow-link codex-codex-wheels-1:/wheels ./cache/packages/
-docker rm codex-codex-wheels-1
+SERVICE_NAME=codex-wheels
+CONTAINER_NAME=project_${SERVICE_NAME}_1
+docker-compose pull ${SERVICE_NAME}
+docker-compose create --no-build ${SERVICE_NAME}
+docker container cp --follow-link ${CONTAINER_NAME}:/wheels ./cache/packages/
+docker rm ${CONTAINER_NAME} || true
