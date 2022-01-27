448dce5728b33e791321434918e32362d5b16e53
AJ Slater <aj@slater.net>
2022-01-26 17:10:15 -0800

    fix docker-version-codex for non circleci

--- docker-version-codex.sh
+++ docker-version-codex.sh
@@ -2,8 +2,9 @@
 # Get the final runnable codex image version
 set -euo pipefail
 source .env
+VERSION=${PKG_VERSION}
 if [ "${CIRCLECI:-}" ]; then
     ARCH=$(./docker-arch.sh)
-    VERSION=${PKG_VERSION}-${ARCH}
+    VERSION=${VERSION}-${ARCH}
 fi
 echo "$VERSION"
