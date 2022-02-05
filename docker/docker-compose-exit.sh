5b9c7a658ec78d97ca8ac73c047c670b422e4a56
AJ Slater <aj@slater.net>
2022-02-05 15:12:42 -0800

    export docker-compose varables

--- docker/docker-compose-exit.sh
+++ docker/docker-compose-exit.sh
@@ -7,5 +7,7 @@ ENV_FN=$(./docker/docker-env-filename.sh)
 source "$ENV_FN"
 export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_BUILDKIT=1
+export CODEX_DIST_BUILDER_VERSION
+export CODEX_BUILDER_FINAL_VERSION
 # docker compose without the dash doesn't have the exit-code-from param
 docker-compose up --exit-code-from "$1" "$1"
