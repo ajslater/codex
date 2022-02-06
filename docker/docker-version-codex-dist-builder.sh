8592d98325e89c1e1a8db5ed9b55fad20293cea7
AJ Slater <aj@slater.net>
2022-02-06 14:40:56 -0800

    use env.build as a duplicate dep

--- docker/docker-version-codex-dist-builder.sh
+++ docker/docker-version-codex-dist-builder.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ENV_FN=$(./docker/docker-env-filename.sh)
 # shellcheck disable=SC1090
 source "$ENV_FN"
-EXTRA_MD5S=("$CODEX_BUILDER_BASE_VERSION  codex-builder-base-version" "$PKG_VERSION  codex-version")
+EXTRA_MD5S=("$CODEX_BUILDER_BASE_VERSION  codex-builder-base-version")
 
 # shellcheck disable=SC2046
 read -ra SOURCE_DEPS <<<$(find codex frontend -type f \( \
@@ -21,6 +21,7 @@ read -ra SOURCE_DEPS <<<$(find codex frontend -type f \( \
 DEPS=(
     "$0"
     .dockerignore
+    .env.build
     .eslintrc.cjs
     .prettierignore
     .remarkignore
