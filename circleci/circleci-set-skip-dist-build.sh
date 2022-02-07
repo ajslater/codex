0d3e5e8190656b7899a64c29c75cce0efe044e7d
AJ Slater <aj@slater.net>
2022-02-06 18:14:12 -0800

    install poetry

--- circleci/circleci-set-skip-dist-build.sh
+++ circleci/circleci-set-skip-dist-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eou pipefail
+pip3 install --upgrade pip
+pip3 install --requirement builder-requirements.txt
 PKG_VERSION=$(./version.sh)
 WHEEL_PATH=dist/codex-${PKG_VERSION}-py3-none-any.whl
 if [ -f "$WHEEL_PATH" ]; then
