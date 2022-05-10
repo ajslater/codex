cb070234439821302bdd908b8d64e07b6cc80106
AJ Slater <aj@slater.net>
2022-05-10 15:30:26 -0700

    make placeholder more generic with adjustable size

--- frontend/src/components/placeholder-loading.vue
+++ frontend/src/components/placeholder-loading.vue
@@ -1,20 +1,12 @@
 <template>
-  <v-progress-circular
-    indeterminate
-    size="128"
-    color="#cc7b19"
-    class="placeholder"
-  />
+  <v-progress-circular indeterminate :size="size" color="#cc7b19" />
 </template>
 
 <script>
 export default {
   name: "PlaceholderLoading",
+  props: {
+    size: { type: Number, default: 128 },
+  },
 };
 </script>
-
-<style scoped lang="scss">
-.placeholder {
-  margin-top: 48px;
-}
-</style>
