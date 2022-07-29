9ece4aa12bba481a013a4731961babcbd51c103a
AJ Slater <aj@slater.net>
2022-07-29 13:28:08 -0700

    lint

--- frontend/src/api/v2/cover.js
+++ frontend/src/api/v2/cover.js
@@ -1,8 +1,10 @@
 import { ROOT_PATH } from "./base";
 
 const MISSING_COVER_PATH = ROOT_PATH + window.missingCover;
-const COVER_PATH_PREFIX = ROOT_PATH + 'api/v2/c';
+const COVER_PATH_PREFIX = ROOT_PATH + "api/v2/c";
 
 export const getCoverSource = (pk, updatedAt) => {
-  return pk ? `${COVER_PATH_PREFIX}/${pk}/cover.webp?${updatedAt}` : MISSING_COVER_PATH;
+  return pk
+    ? `${COVER_PATH_PREFIX}/${pk}/cover.webp?${updatedAt}`
+    : MISSING_COVER_PATH;
 };
