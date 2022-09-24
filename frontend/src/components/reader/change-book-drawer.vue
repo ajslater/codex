b89644345b51ec3437ff0394b1a3fd39bb83ea7e
AJ Slater <aj@slater.net>
2022-09-23 23:51:04 -0700

    make change book drawer fixed. enlarge icon

--- frontend/src/components/reader/change-book-drawer.vue
+++ frontend/src/components/reader/change-book-drawer.vue
@@ -11,7 +11,7 @@
     width="33%"
   >
     <router-link class="navLink" :to="route" :aria-label="label" :title="label">
-      <v-icon class="bookChangeIcon" x-large> {{ icon }} </v-icon>
+      <v-icon class="bookChangeIcon"> {{ icon }} </v-icon>
     </router-link>
   </v-navigation-drawer>
 </template>
@@ -64,6 +64,8 @@ export default {
 </script>
 <style scoped lang="scss">
 .changeBookDrawer {
+  position: fixed;
+  height: 100vh;
   opacity: 0.75;
   z-index: 5;
 }
@@ -78,9 +80,17 @@ export default {
   height: 100%;
 }
 .bookChangeIcon {
-  position: relative;
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
+}
+</style>
+<!-- eslint-disable-next-line vue-scoped-css/enforce-style-type -->
+<style lang="scss">
+$iconSize: 96px;
+.bookChangeIcon svg {
+  height: $iconSize !important;
+  width: $iconSize !important;
+  font-size: $iconSize !important;
 }
 </style>
