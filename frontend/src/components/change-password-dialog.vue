d36fcffac5fba13fdbb1b9d6c15f872b1caf82dc
AJ Slater <aj@slater.net>
2022-12-06 17:17:05 -0800

    html formatting

--- frontend/src/components/change-password-dialog.vue
+++ frontend/src/components/change-password-dialog.vue
@@ -5,17 +5,16 @@
     origin="center-top"
     transition="slide-y-transition"
     max-width="22em"
-    overlay-opacity="0.5"
   >
     <template #activator="{ props }">
       <v-btn v-if="isAdminMode" icon v-bind="props">
         <v-icon> {{ mdiLockPlusOutline }}</v-icon>
       </v-btn>
       <v-list-item v-else v-bind="props">
-        <v-list-item-title
-          ><v-icon>{{ mdiLockReset }}</v-icon
-          >Change Password</v-list-item-title
-        >
+        <v-list-item-title>
+          <v-icon>{{ mdiLockReset }}</v-icon>
+          Change Password
+        </v-list-item-title>
       </v-list-item>
     </template>
     <div v-if="formSuccess" class="codexFormSuccess">
