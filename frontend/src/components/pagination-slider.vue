5a3026c194baebf3946c054a9efe115ce7ddee4b
AJ Slater <aj@slater.net>
2022-12-18 17:07:07 -0800

    lower thumb size of paginzation slider

--- frontend/src/components/pagination-slider.vue
+++ frontend/src/components/pagination-slider.vue
@@ -1,6 +1,6 @@
 <template>
   <v-slider
-    thumb-size="48"
+    thumb-size="36"
     class="paginationSlider"
     density="compact"
     hide-details="auto"
@@ -20,9 +20,15 @@ export default {
 
 <style scoped lang="scss">
 /* Custom slider with a large control. */
+:deep(.v-slider-thumb__ripple) {
+  width: calc(var(--v-slider-thumb-size) * 1.333);
+  height: calc(var(--v-slider-thumb-size) * 1.333);
+  top: -6px;
+  left: -6px;
+}
 :deep(.v-slider-thumb__label) {
   bottom: 0px !important;
-  transform: translateX(-50%) translateY(150%) !important;
+  transform: translateX(-50%) translateY(125%) !important;
   background-color: transparent;
   color: white;
 }
