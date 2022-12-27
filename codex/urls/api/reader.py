ba5528ed50f30d30a9b7d54a495fbb270d4e9c0c
AJ Slater <aj@slater.net>
2022-12-26 21:59:50 -0800

    reorder some stuff

--- codex/urls/api/reader.py
+++ codex/urls/api/reader.py
@@ -10,7 +10,7 @@ from codex.views.reader.session import ReaderSessionView
 
 
 COVER_MAX_AGE = 60 * 60 * 24 * 7
-PAGE_MAX_AGE = 60 * 60 * 24 * 7
+PAGE_MAX_AGE = COVER_MAX_AGE
 TIMEOUT = 60 * 5
 
 app_name = "issue"
