c335a33c31cb06453b1174f4938c1758d4fa6725
AJ Slater <aj@slater.net>
2022-12-28 13:17:50 -0800

    cache control public

--- codex/urls/api/reader.py
+++ codex/urls/api/reader.py
@@ -20,7 +20,7 @@ urlpatterns = [
     # Comic
     path(
         "<int:pk>/cover.webp",
-        cache_control(max_age=COVER_MAX_AGE)(CoverView.as_view()),
+        cache_control(max_age=COVER_MAX_AGE, public=True)(CoverView.as_view()),
         name="cover",
     ),
     path("<int:pk>/download.cbz", DownloadView.as_view(), name="download"),
@@ -30,7 +30,7 @@ urlpatterns = [
     path("<int:pk>", cache_page(TIMEOUT)(ReaderView.as_view()), name="reader"),
     path(
         "<int:pk>/<int:page>/page.jpg",
-        cache_control(max_age=PAGE_MAX_AGE)(ReaderPageView.as_view()),
+        cache_control(max_age=PAGE_MAX_AGE, public=True)(ReaderPageView.as_view()),
         name="page",
     ),
     path("settings", ReaderSessionView.as_view(), name="settings"),
