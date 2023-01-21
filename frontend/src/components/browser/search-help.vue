aae6739a4498bd6a50bc936f6c06d42d7ada0759
AJ Slater <aj@slater.net>
2023-01-21 13:40:35 -0800

    update search help for Whoosh

--- frontend/src/components/browser/search-help.vue
+++ frontend/src/components/browser/search-help.vue
@@ -42,29 +42,49 @@
         </v-table>
       </div>
       <div id="textContainer">
-        <h2>Xapian Query Parser</h2>
+        <h2>Whoosh Query Parser</h2>
         <p>
-          Codex uses the Xapian search backend to execute your text search
-          queries. Xapian operates much like other familiar search engines. As
-          such, I document only some special features on this page. I encourage
-          users looking to understand its full power to read the
-          <a href="https://xapian.org/docs/queryparser.html" target="_blank"
-            >Xapian Query Parser Syntax documentation
+          Codex uses the Whoosh search engine to execute your text search
+          queries. Whoosh operates much like other familiar search engines. As
+          such, only some special features are documented on this page. To
+          understand its full power, users are encouraged to read the
+          <a
+            href="https://whoosh.readthedocs.io/en/latest/querylang.html"
+            target="_blank"
+            >Whoosh Query Language
             <v-icon size="small">{{ mdiOpenInNew }}</v-icon></a
-          >.
+          >
+          documentation.
         </p>
-
+        <h2>Operators</h2>
+        Whoosh has the <code>and</code>, <code>or</code>, <code>not</code>,
+        <code>andnot</code>, <code>andmaybe</code>, and
+        <code>require</code> operators.
         <h2>Searching Fields</h2>
         <p>
-          Like Codex's filter selector menu, but more powerful, you may search
+          Like Codex's filter selector menu, but more powerful. You may search
           on individual comic metadata fields. using a search token like
           <code>field:value</code>.
         </p>
         <h3>Dates and DateTimes</h3>
         <p>
-          Codex parses Dates and DateTime values liberally. If the format you
-          try fails, the YYYYMMDDHHmmSS format is reliable.
+          Whoosh parses Dates and DateTime values liberally. Read
+          <a
+            href="https://whoosh.readthedocs.io/en/latest/dates.html#parsing-date-queries"
+            target="_blank"
+            >Whoosh Parsing Date Queries
+            <v-icon size="small">{{ mdiOpenInNew }}</v-icon>
+          </a>
+          documentation for what formats and natural language phrases are
+          accepted. If the format you try fails, the <code>YYYYMMDD</code> and
+          <code>YYYMMDDHHmmSS</code> formats are reliable.
         </p>
+        <h3>Single Quote complex field Queries</h3>
+        <p>
+          Use <em>single</em> quotes to tell Codex that a complex or natural
+          language phrase should be applied to a specific field
+        </p>
+        <code> created_at:'this year' </code>
         <h3>Size Byte Multipliers</h3>
         <p>
           The size field can make use of convenient byte multiplier suffixes
@@ -74,38 +94,40 @@
         </p>
         <h3>Ranges of Fields</h3>
         <p>
-          Codex uses two dots <code>..</code> to delineate a range search in a
-          field value. If you leave out the upper or lower bound, the wildcard
-          <code>*</code> will tacitily take it's place. Range tokens look like
-          <code>field:lower..upper</code>, <code>field:lower..</code>,
-          <code>field:..upper</code>, <code>field:&gt;lower</code>, or
-          <code>field:&lt;upper</code>
-          Be careful your range search doesn't contain three or more dots. This
-          will cause codex to discard the upper bound value.
+          Whoosh uses brackets and the <em>uppercase</em>&ensp;<code>TO</code>
+          operator to delineate a range search.
         </p>
-
-        <h3>Multi Value Field</h3>
+        <code>created_at:[last sunday TO 10am]</code>.
         <p>
-          Fields marked type <code>CSV</code> may contain comma separated
-          values. This filters by all the supplied values. Multi Value tokens
-          look like:
+          You may leave out the upper or lower bound or use &gt;, &lt;, &gt;=,
+          &lt;= operators.
+        </p>
+        <code>size:&lt;50mb</code>.
+
+        <h3>Multi Value Fields</h3>
+        <p>
+          Fields marked type <code>CSV</code> in the table to the left may
+          contain comma separated values. This filters by all the supplied
+          values. Multi Value tokens look like:
           <code>field:term1,term2,term3</code>
         </p>
-
+        <h2>Grouped Queries</h2>
+        <p>Whoosh allows grouping of queries with parenthesis.</p>
         <h2>Example Search</h2>
         <p>
           This example search is more convoluted than most searches but uses
           many features for demonstration:
         </p>
         <code>
-          Holmes AND Tesla date:>=1999-1-2 size:10mib..1gb Gadzooks NEAR
+          (Holmes and Tesla date:>=1999-1-2 size:[10mib TO 1gb] Gadzooks) or
           "Captain Nemo" -Quartermain
         </code>
         <p style="margin-top: 1em">
           Search for comics that contain Holmes and Tesla published after the
           second day of 1999 that are between 10 Mebibytes and 10 Gigabytes that
-          also contain the word "Gadzooks" near the phrase "Captain Nemo" but do
-          not contain the word "Quartermain".
+          also contain the word "Gadzooks" but also all comics with the the
+          phrase "Captain Nemo" but no comics will contain the word
+          "Quartermain".
         </p>
         <h2>Group and Sorting Behavior</h2>
         <p>
@@ -115,7 +137,6 @@
           different view you may change the group and sort selections after your
           you make your first search.
         </p>
-
         <h2>Filter Behavior</h2>
         <p>
           Codex applies <em>both</em> the Filter selection menu filters
@@ -139,8 +160,8 @@ import { mdiArchiveSearchOutline, mdiOpenInNew } from "@mdi/js";
 import CloseButton from "@/components/close-button.vue";
 
 const FIELD_ROWS = [
-  ["community_rating", "Decimal", ""],
   ["characters", "CSV", "character"],
+  ["community_rating", "Decimal", ""],
   ["country", "String", ""],
   ["created_at", "DateTime", "created"],
   ["creators", "CSV", "creator"],
@@ -206,7 +227,7 @@ export default {
 h1,
 h2,
 h3 {
-  margin-top: 0.25em;
+  margin-top: 0.5em;
   color: rgb(var(--v-theme-textHeader));
 }
 h1 {
