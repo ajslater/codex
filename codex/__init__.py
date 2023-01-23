443cd3330aba116dfef2a1524800f0be433fe742
AJ Slater <aj@slater.net>
2023-01-23 11:59:15 -0800

    move initialization of django to package __init__.py, reduces duplication and makes everything safe.

--- codex/__init__.py
+++ codex/__init__.py
@@ -1,1 +1,9 @@
-"""Init codex package."""
+"""Initialize Django."""
+import os
+
+from codex.signals import connect_signals
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codex.settings.settings")
+connect_signals()
+django.setup()
--- codex/run.py
+++ codex/run.py
@@ -4,8 +4,6 @@ import os
 
 from asyncio import new_event_loop
 
-import django
-
 from django.core.management import call_command
 from hypercorn.asyncio import serve
 
@@ -13,7 +11,7 @@ from codex.asgi import application
 from codex.integrity import rebuild_db, repair_db
 from codex.settings.logging import get_logger
 from codex.settings.settings import DEBUG, HYPERCORN_CONFIG
-from codex.signals import RESTART_EVENT, SHUTDOWN_EVENT, bind_signals, connect_signals
+from codex.signals import RESTART_EVENT, SHUTDOWN_EVENT, bind_signals
 from codex.version import VERSION
 
 
@@ -24,14 +22,11 @@ def set_env():
     """Set environment variables."""
     if DEBUG:
         os.environ["PYTHONDONTWRITEBYTECODE"] = "YES"
-        # Overwritten when we import settings in django.setup()
         LOG.setLevel("DEBUG")
 
 
 def update_db():
     """Update the db to latest migrations."""
-    connect_signals()
-    django.setup()
     call_command("makemigrations", "codex")
     call_command("migrate")
 
