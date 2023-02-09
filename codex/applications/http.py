770a4880483432372bb09fc60bd35e7179652e29
AJ Slater <aj@slater.net>
2023-02-08 18:54:19 -0800

    use path instead of re_path

--- codex/applications/http.py
+++ codex/applications/http.py
@@ -1,6 +1,7 @@
+"""HTPP router for using a channels endpoint."""
 from channels.routing import URLRouter
 from django.core.asgi import get_asgi_application
-from django.urls import re_path
+from django.urls import path, re_path
 
 from codex.applications.websocket import ROOT_PREFIX
 from codex.consumers.send import SendConsumer
@@ -8,8 +9,7 @@ from codex.consumers.send import SendConsumer
 
 HTTP_APPLICATION = URLRouter(
     [
-        # TODO can i use path?
-        re_path(rf"^{ROOT_PREFIX}send", SendConsumer().as_asgi()),
-        re_path(r"", get_asgi_application()),
+        path(f"{ROOT_PREFIX}send", SendConsumer().as_asgi()),  # type: ignore
+        re_path(r"", get_asgi_application()),  # type: ignore
     ]
 )
