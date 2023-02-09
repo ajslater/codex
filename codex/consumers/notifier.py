fd53fa2df912633d786a1e485b836118943575ed
AJ Slater <aj@slater.net>
2023-02-08 19:27:54 -0800

    switch notifier consumer ot AsyncWebsocketConsumer

--- codex/consumers/notifier.py
+++ codex/consumers/notifier.py
@@ -6,7 +6,7 @@ from channels.exceptions import (
     DenyConnection,
     InvalidChannelLayerError,
 )
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 from codex.settings.logging import get_logger
 
@@ -16,23 +16,27 @@ Channels = Enum("Channels", "ALL ADMIN")
 LOG = get_logger(__name__)
 
 
-# TODO change to AsyncWebsockerConsumer
-class NotifierConsumer(AsyncJsonWebsocketConsumer):
+class NotifierConsumer(AsyncWebsocketConsumer):
     """Base Notifier Consumer."""
+
+    @staticmethod
+    def _get_groups(user):
+        """Dynamic groups by user type."""
+        groups = [Channels.ALL.name]
+        if user.is_staff:
+            groups += [Channels.ADMIN.name]
+        LOG.debug(f"Notifier Consumer: Connect to {groups}")
+        return groups
 
     async def websocket_connect(self, _message):
         """Connect websocket to groups."""
         # Authorization
         user = self.scope.get("user")
         if not user:
+            LOG.warning("No websockets without user.")
             return
+        groups = self._get_groups(user)
         try:
-            # Dynamic groups by user type
-            groups = [Channels.ALL.name]
-            if user.is_staff:
-                groups += [Channels.ADMIN.name]
-
-            print(f"Notifier Consumer: Connect to {groups}", self.channel_layer)
             for group in groups:
                 if self.channel_layer:
                     await self.channel_layer.group_add(group, self.channel_name)
@@ -49,8 +53,10 @@ class NotifierConsumer(AsyncJsonWebsocketConsumer):
         except DenyConnection:
             await self.close()
 
-    async def send_message(self, event):
+    async def send_text(self, event):
         """Send message to client."""
-        print("NotifierConsumer.send_message:", event)
-        text = event["text"]
+        text = event.get("text")
+        if not text:
+            LOG.warning("No text in message")
+            return
         await self.send(text)
