f00912e2aefaadbed0302308e889646a9b2edc2a
AJ Slater <aj@slater.net>
2023-02-08 21:55:01 -0800

    comments

--- codex/serializers/websocket_send.py
+++ codex/serializers/websocket_send.py
@@ -1,3 +1,4 @@
+"""Serializer for notifier send endpoint."""
 from rest_framework.serializers import (
     CharField,
     ChoiceField,
@@ -11,11 +12,14 @@ from codex.settings.settings import SECRET_KEY
 
 
 def is_secret(value):
+    """Validate that the secret was passed."""
     if value != SECRET_KEY:
         raise ValidationError("Wrong secret")
 
 
 class SendSerializer(Serializer):
+    """Serializer for notifier messages."""
+
     group = ChoiceField(choices=tuple(e.value for e in Channels))
     text = ChoiceField(WEBSOCKET_MESSAGES)
     secret_key = CharField(validators=[is_secret])
