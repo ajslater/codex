6a44261a541c7cfcb9a1d5c85c2b7b3163efe3ff
AJ Slater <aj@slater.net>
2023-02-16 16:10:58 -0800

    fix tests with env variable

--- test-backend.sh
+++ test-backend.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 ./collectstatic.sh
-poetry run pytest
+BUILD=1 poetry run pytest
 # pytest-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
 poetry run coverage erase || true
