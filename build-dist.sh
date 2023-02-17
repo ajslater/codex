e60b488f106fe1225a719c549328b8e2b83eb5cd
AJ Slater <aj@slater.net>
2023-02-16 20:47:15 -0800

    collect static before check

--- build-dist.sh
+++ build-dist.sh
@@ -3,9 +3,9 @@
 set -euxo pipefail
 cd "$(dirname "$0")"
 
+./collectstatic.sh
 ./pm check
 echo "*** build and package application ***"
 PIP_CACHE_DIR=$(pip3 cache dir)
 export PIP_CACHE_DIR
-./collectstatic.sh
 poetry build
