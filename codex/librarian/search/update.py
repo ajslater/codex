5a7fb0cd91931483c34956808f8b81aefa85a290
AJ Slater <aj@slater.net>
2023-03-01 12:42:34 -0800

    replace search index update selector to gt rather than gte. refactor into smaller methods.

--- codex/librarian/search/update.py
+++ codex/librarian/search/update.py
@@ -8,7 +8,6 @@ from codex.librarian.search.remove import RemoveMixin
 from codex.librarian.search.status import SearchIndexStatusTypes
 from codex.librarian.search.version import VersionMixin
 from codex.models import Comic, Library
-from codex.settings.settings import SEARCH_INDEX_PATH
 
 
 class UpdateMixin(VersionMixin, RemoveMixin):
@@ -28,7 +27,17 @@ class UpdateMixin(VersionMixin, RemoveMixin):
         )
     )
 
+    def _init_statuses(self, rebuild):
+        """Initialize all statuses order before starting."""
+        statuses = {}
+        if rebuild:
+            statuses[SearchIndexStatusTypes.SEARCH_INDEX_CLEAR] = {}
+        statuses.update(self._STATUS_UPDATE_START_TYPES)
+        self.status_controller.start_many(statuses)
+
+
     def _get_search_index_latest_updated_at(self, backend):
+        """Get the date of the last updated item in the search index."""
         if not backend.setup_complete:
             backend.setup()
         backend.index = backend.index.refresh()
@@ -40,6 +49,30 @@ class UpdateMixin(VersionMixin, RemoveMixin):
                 return results[0].get("updated_at")
             else:
                 return None
+
+    def _prepare_for_update(self, rebuild):
+        # Rebuild or set up update
+        backend = self.engine.get_backend()
+
+        if rebuild:
+            self.log.info("Rebuilding search index...")
+            backend.clear(models=[Comic], commit=True)
+            self.status_controller.finish(SearchIndexStatusTypes.SEARCH_INDEX_CLEAR)
+            start_date = None
+        else:
+            start_date = self._get_search_index_latest_updated_at(backend)
+            self.log.info(f"Updating search index since {start_date}...")
+
+        unified_index = self.engine.get_unified_index()
+        index = unified_index.get_index(Comic)
+        # build_queryset does __gte not __gt
+        #qs = index.build_queryset(using=backend, start_date=start_date).order_by(
+        qs = Comic.objects.all()
+        if start_date:
+            qs = qs.filter(updated_at__gt=start_date)
+        qs = qs.order_by("updated_at", "pk")
+
+        return backend, start_date, index, qs
 
     def _update_search_index(self, rebuild=False):
         """Update or Rebuild the search index."""
@@ -58,38 +91,18 @@ class UpdateMixin(VersionMixin, RemoveMixin):
                 self.log.warning("Database does not match search index.")
                 rebuild = True
 
-            statuses = {}
-            if rebuild:
-                statuses[SearchIndexStatusTypes.SEARCH_INDEX_CLEAR] = {}
-            statuses.update(self._STATUS_UPDATE_START_TYPES)
-            self.status_controller.start_many(statuses)
+            self._init_statuses(rebuild)
 
-            SEARCH_INDEX_PATH.mkdir(parents=True, exist_ok=True)
+            backend, start_date, index, qs = self._prepare_for_update(rebuild)
 
-            backend = self.engine.get_backend()
-
-            unified_index = self.engine.get_unified_index()
-            index = unified_index.get_index(Comic)
-
-            if rebuild:
-                self.log.info("Rebuilding search index...")
-                backend.clear(models=[Comic], commit=True)
-                self.status_controller.finish(SearchIndexStatusTypes.SEARCH_INDEX_CLEAR)
-                start_date = None
-            else:
-                start_date = self._get_search_index_latest_updated_at(backend)
-                self.log.info(f"Updating search index since {start_date}...")
-
-            qs = index.build_queryset(using=backend, start_date=start_date).order_by(
-                "updated_at", "pk"
-            )
-
+            # Update
             try:
                 backend.update(index, qs, commit=True)
             except Exception as exc:
                 self.log.error(f"Update search index via backend: {exc}")
                 self.log.exception(exc)
 
+            # Finish
             if rebuild:
                 self._set_search_index_version()
             else:
