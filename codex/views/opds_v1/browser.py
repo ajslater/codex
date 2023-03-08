bb8d9d9e8ee9213f6bf1faebdb51e2bff2b12999
AJ Slater <aj@slater.net>
2023-03-08 12:43:18 -0800

    use top link exclude logic for facets as well

--- codex/views/opds_v1/browser.py
+++ codex/views/opds_v1/browser.py
@@ -260,7 +260,8 @@ class OPDSBrowserView(BrowserView, CodexXMLTemplateView):
             links += self._root_nav_links()
             if self.use_facets:
                 for top_link in TopLinks.ALL:
-                    links += [self._top_link(top_link)]
+                    if not self.is_top_link_displayed(top_link):
+                        links += [self._top_link(top_link)]
                 if facets := self._facets():
                     links += facets
         except Exception as exc:
