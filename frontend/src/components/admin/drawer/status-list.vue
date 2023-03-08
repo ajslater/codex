487840a208acfbbc45f7b55707346641d3c7124d
AJ Slater <aj@slater.net>
2023-03-07 18:25:02 -0800

    format numbers in status list

--- frontend/src/components/admin/drawer/status-list.vue
+++ frontend/src/components/admin/drawer/status-list.vue
@@ -18,7 +18,7 @@
             <div class="statusItemTitle">
               {{ status.type }} {{ status.name }}
               <span v-if="+status.total">
-                {{ status.complete }}/{{ status.total }}
+                {{ nf(status.complete) }} / {{ nf(status.total) }}
               </span>
             </div>
             <v-progress-linear
@@ -38,6 +38,7 @@
 
 <script>
 import { mdiCloseCircleOutline } from "@mdi/js";
+import { numberFormat } from "humanize";
 import { mapActions, mapState } from "pinia";
 
 import CloseButton from "@/components/close-button.vue";
@@ -74,6 +75,9 @@ export default {
     },
     clear() {
       this.librarianTask("librarian_clear_status", "");
+    },
+    nf(val) {
+      return numberFormat(val, 0);
     },
   },
 };
