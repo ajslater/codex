8eddc588fc20cdb3811fe39a179f8160a879f03f
AJ Slater <aj@slater.net>
2023-03-15 18:22:02 -0700

    docker dev container environment

---
+++ restart-docker-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Recreate the codex-dev container and enter it with a shell
+set -euo pipefail
+docker rm -f codex-dev || true
+docker compose down
+docker compose up codex-dev -d
+docker exec -ti codex-dev sh -c 'poetry update; sh'
