e892d1ce7946346eba9e630bf144b2dc722c47f5
AJ Slater <aj@slater.net>
2023-03-17 21:32:50 -0700

    fix delete counting

--- codex/librarian/importer/deleted.py
+++ codex/librarian/importer/deleted.py
@@ -12,10 +12,10 @@ class DeletedMixin(QueuedThread):
     def bulk_folders_deleted(self, library, delete_folder_paths, count, _total):
         """Bulk delete folders."""
         if not delete_folder_paths:
-            return False
+            return count
         query = Folder.objects.filter(library=library, path__in=delete_folder_paths)
-        (num_del, _) = query.delete()
-        count += num_del
+        query.delete()
+        count += len(delete_folder_paths)
         if count:
             level = logging.INFO
         else:
@@ -26,14 +26,14 @@ class DeletedMixin(QueuedThread):
     def bulk_comics_deleted(self, library, delete_comic_paths, count, _total):
         """Bulk delete comics found missing from the filesystem."""
         if not delete_comic_paths:
-            return False
+            return count
         query = Comic.objects.filter(library=library, path__in=delete_comic_paths)
         delete_comic_pks = frozenset(query.values_list("pk", flat=True))
         task = CoverRemoveTask(delete_comic_pks)
         self.librarian_queue.put(task)
 
-        (num_del, _) = query.delete()
-        count += num_del
+        query.delete()
+        count += len(delete_comic_paths)
         if count:
             level = logging.INFO
         else:
