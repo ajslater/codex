721665ea75a83f7eee06c5cccacfdd366fc1a4fd
AJ Slater <aj@slater.net>
2023-03-19 18:55:21 -0700

    fix browser search filter to handle memory errors more gracefully and return partial results

--- codex/views/browser/filters/search.py
+++ codex/views/browser/filters/search.py
@@ -2,61 +2,54 @@
 from django.db.models import Q
 from haystack.query import SearchQuerySet
 
-from codex.librarian.mp_queue import LIBRARIAN_QUEUE
-from codex.librarian.search.tasks import SearchIndexUpdateTask
 from codex.logger.logging import get_logger
 
 LOG = get_logger(__name__)
 
 
-class SearchFilterMixin:  # SearchFilterPreparserMixin):
+class SearchFilterMixin:
     """Search Filters Methods."""
 
-    def _get_search_scores(self, text):
+    def _get_search_scores(self, text, search_scores):
         """Perform the search and return the scores as a dict."""
         sqs = SearchQuerySet().auto_query(text)
         comic_scores = sqs.values("pk", "score")
-        search_scores = {}
-        for comic_score in comic_scores:
-            search_scores[comic_score["pk"]] = comic_score["score"]
+        try:
+            for comic_score in comic_scores:
+                search_scores[comic_score["pk"]] = comic_score["score"]
+        except MemoryError:
+            LOG.warning("Search engine memory error.")
+        except Exception as exc:
+            LOG.warning("While searching:")
+            LOG.exception(exc)
 
-        search_engine_out_of_date = False
-        if search_engine_out_of_date:
-            LOG.warning("Search index out of date. Scoring non-existent comics.")
-            task = SearchIndexUpdateTask(False)
-            LIBRARIAN_QUEUE.put(task)
-        return search_scores
-
-    def _get_search_query_filter(self, text, is_model_comic):
+    def _get_search_query_filter(
+            self, text, is_model_comic, search_scores
+        ):
         """Get the search filter and scores."""
-        search_filter = Q()
-        search_scores = {}
-        if text:
-            # Get search scores
-            search_scores = self._get_search_scores(text)
+        # Get search scores
+        self._get_search_scores(text, search_scores)
 
-            # Create query
-            prefix = ""
-            if not is_model_comic:
-                prefix = "comic__"
-            query_dict = {f"{prefix}pk__in": search_scores.keys()}
-            search_filter = Q(**query_dict)
+        # Create query
+        prefix = ""
+        if not is_model_comic:
+            prefix = "comic__"
+        query_dict = {f"{prefix}pk__in": search_scores.keys()}
+        return Q(**query_dict)
 
-        return search_filter, search_scores
 
     def get_search_filter(self, is_model_comic):
         """Preparse search, search and return the filter and scores."""
+        search_filter = Q()
+        search_scores = {}
         try:
             query_string = self.params.get("q", "")  # type: ignore
 
-            # Query haystack
-            (
-                search_filter,
-                search_scores,
-            ) = self._get_search_query_filter(query_string, is_model_comic)
+            if query_string:
+                # Query haystack
+                search_filter = self._get_search_query_filter(
+                    query_string, is_model_comic, search_scores)
         except Exception as exc:
             LOG.warning(exc)
-            search_filter = Q()
-            search_scores = {}
 
         return search_filter, search_scores
