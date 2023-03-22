5974ce2d358d1c9208b1321c7239fc9e588329c2
AJ Slater <aj@slater.net>
2023-03-21 19:07:08 -0700

    lint

--- codex/librarian/watchdog/observers.py
+++ codex/librarian/watchdog/observers.py
@@ -1,11 +1,11 @@
 """The Codex Library Watchdog Observer threads."""
+from watchdog.observers import Observer
 from watchdog.observers.api import (
     DEFAULT_OBSERVER_TIMEOUT,
     BaseObserver,
     ObservedWatch,
 )
 
-from watchdog.observers import Observer
 from codex.librarian.watchdog.emitter import DatabasePollingEmitter
 from codex.librarian.watchdog.events import CodexLibraryEventHandler
 from codex.models import Library
@@ -117,7 +117,7 @@ class UatuMixin(BaseObserver, WorkerBaseMixin):
 # It would be best for Codex to have one observer with multiple emitters, but the
 #     watchdog class structure doesn't work that way.
 
-class LibraryEventObserver(UatuMixin, Observer):
+class LibraryEventObserver(UatuMixin, Observer): # type: ignore
     """Regular observer."""
 
     ENABLE_FIELD = "events"
