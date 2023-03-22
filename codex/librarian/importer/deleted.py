20a9a68a3f39e4cfc66cc16e60fd0790588c2a4f
AJ Slater <aj@slater.net>
2023-03-21 19:21:16 -0700

    count comics and remove covers when folders are deleted.

--- codex/librarian/importer/deleted.py
+++ codex/librarian/importer/deleted.py
@@ -9,15 +9,30 @@ from codex.threads import QueuedThread
 class DeletedMixin(QueuedThread):
     """Clean up database methods."""
 
+    def _remove_covers(self, delete_comic_pks):
+        task = CoverRemoveTask(delete_comic_pks)
+        self.librarian_queue.put(task)
+
     @status(status=ImportStatusTypes.DIRS_DELETED, updates=False)
     def bulk_folders_deleted(self, delete_folder_paths, library, status_args=None):
         """Bulk delete folders."""
         if not delete_folder_paths:
             return 0
-        query = Folder.objects.filter(library=library, path__in=delete_folder_paths)
-        query.delete()
+        folders = Folder.objects.filter(library=library, path__in=delete_folder_paths)
+        delete_comic_pks = frozenset(
+            Comic.objects.filter(library=library, folders__in=folders)
+            .distinct()
+            .values_list("pk", flat=True)
+        )
+        folders.delete()
+
+        self._remove_covers(delete_comic_pks)
+
         count = len(delete_folder_paths)
-        self.log.info(f"Deleted {count} folders from {library.path}")
+        self.log.info(
+            f"Deleted {count} folders and {len(delete_comic_pks)} comics"
+            f"from {library.path}"
+        )
         return count
 
     @status(status=ImportStatusTypes.FILES_DELETED, updates=False)
@@ -25,12 +40,15 @@ class DeletedMixin(QueuedThread):
         """Bulk delete comics found missing from the filesystem."""
         if not delete_comic_paths:
             return 0
-        query = Comic.objects.filter(library=library, path__in=delete_comic_paths)
-        delete_comic_pks = frozenset(query.values_list("pk", flat=True))
-        task = CoverRemoveTask(delete_comic_pks)
-        self.librarian_queue.put(task)
+        comics = Comic.objects.filter(library=library, path__in=delete_comic_paths)
+        delete_comic_pks = frozenset(comics.values_list("pk", flat=True))
+        comics.delete()
 
-        query.delete()
+        self._remove_covers(delete_comic_pks)
+
         count = len(delete_comic_paths)
-        self.log.info(f"Deleted {count} comics from {library.path}")
+        self.log.info(
+            f"Deleted {count} comics from {library.path}"
+        )
+
         return count
