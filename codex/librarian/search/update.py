267698a484b5030ed31197f9d9310be3311b6090
AJ Slater <aj@slater.net>
2023-03-30 01:08:41 -0700

    fix logging

--- codex/librarian/search/update.py
+++ codex/librarian/search/update.py
@@ -176,11 +176,11 @@ class UpdateMixin(RemoveMixin):
                 self.status_controller.update(status)
             except Exception as exc:
                 retry_batches[retry_batch_num] = batch_pks
-                retry_batch_num += 1
                 self.log.warning(
                     f"Search index update will retry batch {batch_num}"
-                    f"as new batch {retry_batch_num}."
+                    f" as new batch {retry_batch_num}."
                 )
+                retry_batch_num += 1
                 if not isinstance(exc, self._EXPECTED_EXCEPTIONS):
                     self.log.exception("Search index update - collect batch results")
         return retry_batches, complete
