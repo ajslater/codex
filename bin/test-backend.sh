23d750ca999512c15dba9c3c7dcb6d5d5b3e07fd
AJ Slater <aj@slater.net>
2023-04-13 12:32:48 -0700

    fix tests

--- bin/test-backend.sh
+++ bin/test-backend.sh
@@ -2,8 +2,7 @@
 # Run all codex tests
 set -euxo pipefail
 
-cd "$(dirname "$0")"
-
+export PYTHONPATH=.
 BUILD=1 poetry run pytest
 # pytest-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
 poetry run coverage erase || true
