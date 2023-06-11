cada794afed3c88403bc6afc9d5d0a005067499d
AJ Slater <aj@slater.net>
2023-06-10 22:53:50 -0700

    fix top folder view and force folder & story arc url to have proper top_group

--- codex/views/browser/filters/group.py
+++ codex/views/browser/filters/group.py
@@ -10,14 +10,16 @@ class GroupFilterMixin(GroupACLMixin):
     def get_group_filter(self, choices):
         """Get filter for the displayed group."""
         pk = self.kwargs.get("pk")  # type: ignore
+        group = self.kwargs.get("group")  # type: ignore
         if pk:
-            group = self.kwargs.get("group")  # type: ignore
             group_relation = "comic__" if choices else ""
             if choices and group == self.FOLDER_GROUP:
                 group_relation += "folders"
             else:
                 group_relation += self.GROUP_RELATION[group]
             group_filter = Q(**{group_relation: pk})
+        elif group == self.FOLDER_GROUP:
+            group_filter = Q(parent_folder=None)
         else:
             group_filter = Q()
 
