165fe97bbf79fb492ed9eb737c903590f45aba59
AJ Slater <aj@slater.net>
2024-03-19 19:36:26 -0700

    move ngix config to docker dir

--- bin/prettier-nginx.sh
+++ bin/prettier-nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run prettier on nginx files because overrides doesn't work yet.
 set -euo pipefail
-CONFIG_DIR=bin/nginx/
+CONFIG_DIR=docker/nginx/
 prettier --parser nginx "$CONFIG_DIR"/http.d/codex/*.conf "$@"
