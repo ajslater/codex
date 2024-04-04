3ca9ee85f84ac7e3e601ebacf58171aa7602f7b0
AJ Slater <aj@slater.net>
2024-04-04 11:19:59 -0700

    remove workaround for channels bug

--- codex/applications/websocket.py
+++ codex/applications/websocket.py
@@ -5,20 +5,14 @@ from channels.routing import URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path
 
-from codex.settings.settings import ROOT_PATH
 from codex.websockets.consumers import NotifierConsumer
-
-# channels is ignorant of root_path
-# https://github.com/django/channels/issues/1973
-ROOT_PREFIX = ROOT_PATH[1:] + "/" if ROOT_PATH else ""
-
 
 WEBSOCKET_APPLICATION = AllowedHostsOriginValidator(
     AuthMiddlewareStack(
         URLRouter(
             [
                 path(
-                    f"{ROOT_PREFIX}api/v3/ws",
+                    "api/v3/ws",
                     NotifierConsumer.as_asgi(),
                     name="websocket",
                 ),
