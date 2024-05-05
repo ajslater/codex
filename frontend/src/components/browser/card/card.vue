d623fc8837267169250c96c17eeb867e61ec578f
AJ Slater <aj@slater.net>
2024-05-04 22:37:20 -0700

    fix card button clicked color being all white"

--- frontend/src/components/browser/card/card.vue
+++ frontend/src/components/browser/card/card.vue
@@ -164,6 +164,7 @@ export default {
   border-color: rbg(var(--v-theme-primary));
 }
 .browserCardCoverWrapper:hover > .browserCardTop > .cardCoverOverlay * {
+  background-color: transparent;
   opacity: 1;
 }
 .bookCoverProgress {
