65ea45fc776ce0246770d40222ae8c13bba6bb2e
AJ Slater <aj@slater.net>
2024-05-23 12:19:28 -0700

    reduce filter batch size

--- codex/settings/settings.py
+++ codex/settings/settings.py
@@ -37,10 +37,11 @@ WRITER_MEMORY_PERCENT = float(environ.get("CODEX_WRITER_MEMORY_PERCENT", 0.6))
 CPU_MULTIPLIER = float(environ.get("CODEX_CPU_MULTIPLIER", 1.25))
 CHUNK_PER_GB = int(environ.get("CODEX_CHUNK_PER_GB", 250))
 MAX_CHUNK_SIZE = int(environ.get("CODEX_MAX_CHUNK_SIZE", 1000))
-# sqlite parser breaks with more than 1000 variables in a query and django only
-# fixes this in the bulk_create & bulk_update functions. So for complicated
-# queries I gotta batch them myself. Filter arg count is a proxy, but it works.
-FILTER_BATCH_SIZE = int(environ.get("CODEX_FILTER_BATCH_SIZE", 990))
+# sqlite parser breaks with more than 1000 variables in a query and
+# django only fixes this in the bulk_create & bulk_update functions.
+# So for complicated queries I gotta batch them myself. Filter arg
+# count is a proxy, but it works. 990 errors sometimes.
+FILTER_BATCH_SIZE = int(environ.get("CODEX_FILTER_BATCH_SIZE", 900))
 VITE_HOST = environ.get("VITE_HOST")
 
 ####################################
