743f6ab2671e3453b0822fa2a2ff871d3240bdc4
AJ Slater <aj@slater.net>
2024-06-09 18:49:53 -0700

    reduce complexity of browser group filter

--- codex/views/browser/filters/group.py
+++ codex/views/browser/filters/group.py
@@ -12,6 +12,26 @@ class GroupFilterView(SessionView):
 
     SESSION_KEY = SessionView.BROWSER_SESSION_KEY
 
+    def _get_rel_for_pks(self, model, group):
+        """Get the relation from the model to the pks."""
+        # XXX these TARGET refs might be better as subclass get rel methods.
+        target: str = self.TARGET  # type: ignore
+        if (
+            target in frozenset({"choices", "cover"})
+            and model == Comic
+            and group == FOLDER_GROUP
+        ):
+            rel = "folders"
+        elif target in frozenset({"metadata", "mtime"}) or (
+            target == "cover" and model != Comic
+        ):
+            rel = "pk"
+        else:
+            rel = GROUP_RELATION[group]
+
+        rel += "__in"
+        return rel
+
     def get_group_filter(self, model, group=None, pks=None):
         """Get filter for the displayed group."""
         if group is None:
@@ -19,23 +39,9 @@ class GroupFilterView(SessionView):
         if pks is None:
             pks = self.kwargs["pks"]
 
-        if pks and 0 not in pks:  # type: ignore
-            target: str = self.TARGET  # type: ignore
-            if (
-                target in frozenset({"choices", "cover"})
-                and model == Comic
-                and group == FOLDER_GROUP
-            ):
-                rel = "folders"
-            elif target in frozenset({"metadata", "mtime"}) or (
-                target == "cover" and model != Comic
-            ):
-                rel = "pk"
-            else:
-                rel = GROUP_RELATION[group]
-
-            rel += "__in"
-            group_filter_dict = {rel: pks}  # type: ignore
+        if pks and 0 not in pks:
+            rel = self._get_rel_for_pks(model, group)
+            group_filter_dict = {rel: pks}
         elif group == FOLDER_GROUP:
             group_filter_dict = {"parent_folder": None}
         else:
