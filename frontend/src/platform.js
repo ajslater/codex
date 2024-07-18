28e2d295a02bbbcbea6e6ff93f129d46d4b82bd2
AJ Slater <aj@slater.net>
2024-07-18 14:15:02 -0700

    simplify IOS detection

--- frontend/src/platform.js
+++ frontend/src/platform.js
@@ -5,9 +5,8 @@ export const IS_TOUCH =
   navigator.msMaxTouchPoints > 0 ||
   window.matchMedia("(any-hover: none)").matches;
 const IOS_PLATFORMS = ["iPad", "iPhone", "iPod"];
-export const IS_IOS = IOS_PLATFORMS.some((word) =>
-  navigator.userAgent.includes(word),
-);
-const IS_MAC = navigator.userAgent.includes("Mac OS");
-export const IS_IOS_DESKTOP_MODE = IS_MAC && IS_TOUCH;
-export default { IS_IOS, IS_TOUCH, IS_IOS_DESKTOP_MODE };
+export const IS_IOS =
+  IOS_PLATFORMS.some((word) => navigator.userAgent.includes(word)) ||
+  // iOS in Desktop mode
+  (IS_TOUCH && navigator.userAgent.includes("Mac OS"));
+export default { IS_IOS, IS_TOUCH };
