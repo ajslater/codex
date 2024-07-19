d1ec8816d0607557fe5771b5c2604e6b8afad092
AJ Slater <aj@slater.net>
2024-07-18 21:09:42 -0700

    minor reordering of variables for platform.js

--- frontend/src/platform.js
+++ frontend/src/platform.js
@@ -1,12 +1,12 @@
 // Identify platform for special behaviors
+const IOS_PLATFORMS = ["iPad", "iPhone", "iPod"];
+Object.freeze(IOS_PLATFORMS);
 export const IS_TOUCH =
   "ontouchstart" in window ||
   navigator.maxTouchPoints > 0 ||
   navigator.msMaxTouchPoints > 0 ||
   window.matchMedia("(any-hover: none)").matches;
-const IOS_PLATFORMS = ["iPad", "iPhone", "iPod"];
 export const IS_IOS =
   IOS_PLATFORMS.some((word) => navigator.userAgent.includes(word)) ||
-  // iOS in Desktop mode
   (IS_TOUCH && navigator.userAgent.includes("Mac OS"));
 export default { IS_IOS, IS_TOUCH };
