78e24f0a438b33a103bc027e0ce58550b39eec90
AJ Slater <aj@slater.net>
2024-08-06 18:22:58 -0700

    move field aliases dict out of search backend.

--- codex/search/backend.py
+++ codex/search/backend.py
@@ -37,6 +37,7 @@ from codex.logger.logging import get_logger
 from codex.memory import get_mem_limit
 from codex.models import AdminFlag, Comic
 from codex.search.backend_search import CodexBackendSearchMixin
+from codex.search.fields import FIELDMAP
 from codex.search.indexes import ComicIndex
 from codex.search.writing import CodexWriter
 from codex.settings.settings import (
@@ -47,23 +48,6 @@ from codex.settings.settings import (
     WRITER_MEMORY_PERCENT,
 )
 from codex.worker_base import WorkerBaseMixin
-
-
-def gen_multipart_field_aliases(field):
-    """Generate aliases for fields made of snake_case words."""
-    bits = field.split("_")
-    aliases = []
-
-    # Singular from plural
-    if field.endswith("s"):
-        aliases += [field[:-1]]
-
-    # Alternate delimiters
-    for connector in ("", "-"):
-        joined = connector.join(bits)
-        aliases += [joined, joined[:-1]]
-
-    return tuple(sorted(frozenset(aliases)))
 
 
 class FILESIZE(NUMERIC):
@@ -107,49 +91,6 @@ TEXT_ANALYZER = (
 class CodexSearchBackend(CodexBackendSearchMixin, WhooshSearchBackend, WorkerBaseMixin):
     """Custom Whoosh Backend."""
 
-    FIELDMAP = MappingProxyType(
-        {
-            "characters": ("category", "character"),
-            "community_rating": gen_multipart_field_aliases("community_rating"),
-            "contributors": (
-                *gen_multipart_field_aliases("authors"),
-                "contributor",
-                *gen_multipart_field_aliases("creators"),
-                *gen_multipart_field_aliases("credits"),
-                *gen_multipart_field_aliases("people"),
-                *gen_multipart_field_aliases("persons"),
-            ),
-            "created_at": ("created",),
-            "critical_rating": gen_multipart_field_aliases("critical_rating"),
-            "genres": ("genre",),
-            "file_type": (
-                "filetype",
-                "type",
-            ),
-            "identifier": ("id", "nss"),
-            "identifier_type": ("idtype", "id_type", "nid"),
-            "issue_number": ("number",),
-            "locations": ("location",),
-            "monochrome": gen_multipart_field_aliases("black_and_white"),
-            "name": ("title",),
-            "original_format": ("format",),
-            "page_count": ("pages",),
-            "reading_direction": ("direction",),
-            "search_path": (
-                "path",
-                "filename",
-                *gen_multipart_field_aliases("folders"),
-            ),
-            "series_groups": gen_multipart_field_aliases("series_groups"),
-            "scan_info": ("scan",),
-            "stories": ("story",),
-            "story_arcs": gen_multipart_field_aliases("story_arcs"),
-            "summary": ("comments", "description"),
-            "tags": ("tag",),
-            "teams": ("team",),
-            "updated_at": ("updated",),
-        }
-    )
     RESERVED_CHARACTERS = ()
     RESERVED_WORDS = ()
     FIELD_ALIAS_PLUGIN = FieldAliasPlugin(FIELDMAP)
@@ -204,8 +145,8 @@ class CodexSearchBackend(CodexBackendSearchMixin, WhooshSearchBackend, WorkerBas
     _CPU_MULTIPLIER = CPU_MULTIPLIER
     _CHUNK_PER_GB = CHUNK_PER_GB
     _MAX_CHUNK_SIZE = MAX_CHUNK_SIZE
-    _DECIMAL_FIELD_NAMES = frozenset({"decimal", "float"})
-    _DATETIME_FIELD_NAMES = frozenset({"date", "datetime"})
+    _DECIMAL_FIELD_TYPES = frozenset({"decimal", "float"})
+    _DATETIME_FIELD_TYPES = frozenset({"date", "datetime"})
 
     def __init__(self, connection_alias, **connection_options):
         """Init worker queues."""
@@ -256,13 +197,13 @@ class CodexSearchBackend(CodexBackendSearchMixin, WhooshSearchBackend, WorkerBas
                     signed=False,
                     stored=field_class.stored,
                 )
-            elif field_class.field_type in self._DECIMAL_FIELD_NAMES:
+            elif field_class.field_type in self._DECIMAL_FIELD_TYPES:
                 schema_fields[index_fieldname] = NUMERIC(
                     numtype=float,  # type: ignore
                     field_boost=field_class.boost,
                     stored=field_class.stored,
                 )
-            elif field_class.field_type in self._DATETIME_FIELD_NAMES:
+            elif field_class.field_type in self._DATETIME_FIELD_TYPES:
                 schema_fields[index_fieldname] = DATETIME(
                     stored=field_class.stored,
                 )
---
+++ codex/search/fields.py
@@ -0,0 +1,65 @@
+"""Search query fields."""
+
+from types import MappingProxyType
+
+
+def gen_multipart_field_aliases(field):
+    """Generate aliases for fields made of snake_case words."""
+    bits = field.split("_")
+    aliases = []
+
+    # Singular from plural
+    if field.endswith("s"):
+        aliases += [field[:-1]]
+
+    # Alternate delimiters
+    for connector in ("", "-"):
+        joined = connector.join(bits)
+        aliases += [joined, joined[:-1]]
+
+    return tuple(sorted(frozenset(aliases)))
+
+
+FIELDMAP = MappingProxyType(
+    {
+        "characters": ("category", "character"),
+        "community_rating": gen_multipart_field_aliases("community_rating"),
+        "contributors": (
+            *gen_multipart_field_aliases("authors"),
+            "contributor",
+            *gen_multipart_field_aliases("creators"),
+            *gen_multipart_field_aliases("credits"),
+            *gen_multipart_field_aliases("people"),
+            *gen_multipart_field_aliases("persons"),
+        ),
+        "created_at": ("created",),
+        "critical_rating": gen_multipart_field_aliases("critical_rating"),
+        "genres": ("genre",),
+        "file_type": (
+            "filetype",
+            "type",
+        ),
+        "identifier": ("id", "nss"),
+        "identifier_type": ("idtype", "id_type", "nid"),
+        "issue_number": ("number",),
+        "locations": ("location",),
+        "monochrome": gen_multipart_field_aliases("black_and_white"),
+        "name": ("title",),
+        "original_format": ("format",),
+        "page_count": ("pages",),
+        "reading_direction": ("direction",),
+        "search_path": (
+            "path",
+            "filename",
+            *gen_multipart_field_aliases("folders"),
+        ),
+        "series_groups": gen_multipart_field_aliases("series_groups"),
+        "scan_info": ("scan",),
+        "stories": ("story",),
+        "story_arcs": gen_multipart_field_aliases("story_arcs"),
+        "summary": ("comments", "description"),
+        "tags": ("tag",),
+        "teams": ("team",),
+        "updated_at": ("updated",),
+    }
+)
