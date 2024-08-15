bfc15af1014550bf9678212e29f193360869a3fd
AJ Slater <aj@slater.net>
2024-08-15 13:53:43 -0700

    fix preparsing for FTS language

--- codex/views/browser/filters/search/parse.py
+++ codex/views/browser/filters/search/parse.py
@@ -33,8 +33,12 @@ _NON_FTS_COLUMNS = frozenset(
     }
 )
 _VALID_COLUMNS = frozenset(_FTS_COLUMNS | _NON_FTS_COLUMNS)
-_EXPRESSION_OPERATOR_RE = re.compile(r"^[\*\!\<\>]")
-_FTS_OPERATORS = frozenset({"or", "and", "not", "near"})
+_COLUMN_EXPRESSION_OPERATORS_RE = re.compile(r"^[\*\!\<\>]")
+_FTS_OPERATORS = frozenset({"and", "not", "or", "near"})
+_FTS_BARE_TOKENS = "(),"
+_FTS_PAREN_COMMAS_RE = re.compile(r"(\(|\)|,)")
+_FTS_MULTI_COL_RE = re.compile(r"{.*}:")
+_QUOTED_RE = re.compile(r'^".*"$|^\'.*\'$')
 
 
 class SearchFilterView(BrowserFTSFilter):
@@ -44,18 +48,36 @@ class SearchFilterView(BrowserFTSFilter):
         """Preparse one search token."""
         if not token:
             return
-        parts = token.split(":")
-        if len(parts) > 1:
-            col, exp = parts
-            if col not in _VALID_COLUMNS:
-                return
-            if col in _NON_FTS_COLUMNS or _EXPRESSION_OPERATOR_RE.search(exp):
-                field_tokens.add((col, exp))
-                return
 
-        # Allow lowercase FTS operators
-        cased_token = token.upper() if token.lower() in _FTS_OPERATORS else token
-        fts_tokens.append(cased_token)
+        if token.lower() in _FTS_OPERATORS:
+            # Allow lowercase FTS operators
+            token = token.upper()
+        elif (
+            token not in _FTS_BARE_TOKENS
+            and not token.isdigit()
+            and not _QUOTED_RE.search(token)
+        ):
+            column_parts = token.split(":")
+            if len(column_parts) > 1:
+                # Column token
+                col, exp = column_parts
+                if col not in _VALID_COLUMNS:
+                    return
+                if col in _NON_FTS_COLUMNS or _COLUMN_EXPRESSION_OPERATORS_RE.search(exp):
+                    field_tokens.add((col, exp))
+                    return
+                # Else send the column token to fts
+            elif _FTS_PAREN_COMMAS_RE.search(token):
+                # Not a column token, but has grouping chars
+                # Surround parents and commas with spaces and re shlex and parse them.
+                sub_phrase = _FTS_PAREN_COMMAS_RE.sub(" \1 ", token)
+                tokens = shlex.split(sub_phrase)
+                for sub_token in tokens:
+                    self._preparse_token(sub_token, field_tokens, fts_tokens)
+                return
+            token = f'"{token}"'
+
+        fts_tokens.append(token)
 
     def _preparse_search_query(self):
         """Preparse search fields out of query text."""
@@ -64,13 +86,17 @@ class SearchFilterView(BrowserFTSFilter):
         if not q:
             return field_tokens, q
 
+        # Remove multi column specifiers rather than accommodating them.
+        # For simplicity
+        q = _FTS_MULTI_COL_RE.sub("", q)
+
         tokens = shlex.split(q)
         fts_tokens = []
         for token in tokens:
             self._preparse_token(token, field_tokens, fts_tokens)
 
-        preparsed_search_query = " ".join(fts_tokens).strip()
-        return field_tokens, preparsed_search_query
+        text = " ".join(fts_tokens)
+        return field_tokens, text
 
     def apply_search_filter(self, qs, model):
         """Preparse search, search and return the filter and scores."""
