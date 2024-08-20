3479a49b34eadc82da8fe8c7d59a664e0a32029e
AJ Slater <aj@slater.net>
2024-08-19 21:19:11 -0700

    remove cruft

--- codex/signals/django_signals.py
+++ codex/signals/django_signals.py
@@ -36,13 +36,7 @@ def _user_group_change(**kwargs):
     LIBRARIAN_QUEUE.put(task)
 
 
-# def _cache_invalidated(sender, **kwargs):
-#    """For cache debugging."""
-#    print(f"invalidated signal {sender}")
-
-
 def connect_signals():
     """Connect actions to signals."""
     # connection_created.connect(_db_connect)
     m2m_changed.connect(_user_group_change)
-    # post_invalidation.connect(_cache_invalidated)
