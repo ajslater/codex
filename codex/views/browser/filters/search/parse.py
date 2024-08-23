7f2d2e3b535f966ca3fa791596e05ac06ffa7e4a
AJ Slater <aj@slater.net>
2024-08-23 12:58:52 -0700

    replace shlex tokenizer with custom regex tokenizer, handles parens

--- codex/views/browser/filters/search/parse.py
+++ codex/views/browser/filters/search/parse.py
@@ -1,10 +1,10 @@
 """Search Filters Methods."""
 
 import re
-import shlex
 
 from codex.logger.logging import get_logger
 from codex.models.comic import ComicFTS
+from codex.views.browser.filters.search.aliases import ALIAS_FIELD_MAP
 from codex.views.browser.filters.search.fts import BrowserFTSFilter
 
 LOG = get_logger(__name__)
@@ -34,98 +34,70 @@ _NON_FTS_COLUMNS = frozenset(
 _VALID_COLUMNS = frozenset(_FTS_COLUMNS | _NON_FTS_COLUMNS)
 _COLUMN_EXPRESSION_OPERATORS_RE = re.compile(r"\B[\*\!\<\>]\w|\.{2,}|\w\*\w")
 _FTS_OPERATORS = frozenset({"and", "not", "or", "near"})
-_FTS_BARE_TOKENS = "(),"
-_FTS_PAREN_COMMAS_RE = re.compile(r"([()|])")
-_FTS_MULTI_COL_RE = re.compile(r"{.*}:")
-_QUOTED_RE = re.compile(r'^".*"$|^\'.*\'$')
+_FTS_OPERATOR_REXP = rf"(?P<operator>\b{'|'.join(_FTS_OPERATORS)}\b)"
+_FTS_OPERATOR_RE = re.compile(_FTS_OPERATOR_REXP, flags=re.IGNORECASE)
+_MULTI_COL_REXP = r"(?P<multi_col>\{.*?\})"
+_SINGLE_COL_REXP = r"(?P<col>\w+)"
+_EXP_REXP = r"(?P<exp>\s*\(.*?\)|\S+)"
+_COL_REXP = rf"({_MULTI_COL_REXP}|{_SINGLE_COL_REXP}):{_EXP_REXP}"
+_TOKEN_REXP = rf"(?P<token>{_COL_REXP}|\S+)"
+_TOKEN_RE = re.compile(_TOKEN_REXP, flags=re.IGNORECASE)
 
 
 class SearchFilterView(BrowserFTSFilter):
     """Search Query Parser."""
 
-    def _preparse_paren_commas(self, token, field_tokens, fts_tokens):
-        """Not a column token, but has grouping chars."""
-        # Surround parents and commas with spaces and re shlex and parse them.
-        sub_phrase = _FTS_PAREN_COMMAS_RE.sub(r" \g<1> ", token)
-        tokens = shlex.split(sub_phrase)
-        for sub_token in tokens:
-            self._preparse_token(sub_token, field_tokens, fts_tokens)
-
-    @staticmethod
-    def _quote_token(token):
-        """Quote most tokens to allow special characters."""
-        # but preserve prefix star notation
-        suffix = ""
-        while token.endswith("*"):
-            suffix = "*"
-            token = token[:-1]
-        return f'"{token}"{suffix}'
-
-    def _preparse_column_search(self, token, field_tokens, fts_tokens):
-        """Preparse column search."""
-        column_parts = token.split(":", 1)
-        if len(column_parts) <= 1:
-            return False
-        # Column token
-        col, exp = column_parts
+    def _parse_column_match(self, col, exp, field_tokens):  # , fts_tokens):
+        col = ALIAS_FIELD_MAP.get(col, col)
         if col not in _VALID_COLUMNS:
+            # print("not valid col", col)
             return True
         if col == "path" and not self.is_admin():  # type: ignore
             if not self.admin_flags:  # type: ignore
                 # Ensure admin flags for Cover View
                 self.set_admin_flags()  # type: ignore
             if not self.admin_flags["folder_view"]:  # type: ignore
+                # print("path denied")
                 return True
-        # TODO test column expression regex. May require full binary logic parse for tokens.
         if col in _NON_FTS_COLUMNS or _COLUMN_EXPRESSION_OPERATORS_RE.search(exp):
+            # print("add to field", col, exp)
             field_tokens.add((col, exp))
             return True
 
-        exp = self._quote_token(exp)
-        token = ":".join((col, exp))
-        fts_tokens.append(token)
-        return True
+        return False
 
-    def _preparse_token(self, token, field_tokens, fts_tokens):
-        """Preparse one search token."""
-        if not token:
-            return
-
-        if token.lower() in _FTS_OPERATORS:
-            # Allow lowercase FTS operators
-            token = token.upper()
-        elif (
-            token not in _FTS_BARE_TOKENS
-            and not token.isdigit()
-            and not _QUOTED_RE.search(token)
-        ):
-            if self._preparse_column_search(token, field_tokens, fts_tokens):
-                return
-                # Else send the column token to fts
-            if _FTS_PAREN_COMMAS_RE.search(token):
-                self._preparse_paren_commas(token, field_tokens, fts_tokens)
-                return
-            token = self._quote_token(token)
-
+    @staticmethod
+    def _add_fts_token(fts_tokens, token):
+        token = _FTS_OPERATOR_RE.sub(lambda op: op.group("operator").upper(), token)
         fts_tokens.append(token)
 
     def _preparse_search_query(self):
         """Preparse search fields out of query text."""
-        q = self.params.get("q")  # type: ignore
+        text = self.params.get("q")  # type: ignore
         field_tokens = set()
-        if not q:
-            return field_tokens, q
+        if not text:
+            return field_tokens, text
 
-        # Remove multi column specifiers rather than accommodating them.
-        # For simplicity
-        q = _FTS_MULTI_COL_RE.sub("", q)
-
-        tokens = shlex.split(q)
         fts_tokens = []
-        for token in tokens:
-            self._preparse_token(token, field_tokens, fts_tokens)
+        for match in _TOKEN_RE.finditer(text):
+            token = match.group("token")
+            if not token:
+                continue
 
-        text = " ".join(fts_tokens)
+            multi_col = match.group("multi_col")
+            col = match.group("col")
+            exp = match.group("exp")
+            if multi_col or not col or not exp:
+                # XXX Could add mulit-col to field groups, but nobody will care.
+                self._add_fts_token(fts_tokens, token)
+                continue
+
+            if not self._parse_column_match(col, exp, field_tokens):  # , fts_tokens):
+                self._add_fts_token(fts_tokens, token)
+
+        if fts_tokens:
+            text = " ".join(fts_tokens)
+
         return field_tokens, text
 
     def apply_search_filter(self, qs, model):
