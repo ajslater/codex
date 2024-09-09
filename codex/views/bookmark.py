f145806771a976f937644f89db1da2a17e4c44ca
AJ Slater <aj@slater.net>
2024-09-09 15:22:47 -0700

    simplify bookmark update detection

--- codex/views/bookmark.py
+++ codex/views/bookmark.py
@@ -74,9 +74,9 @@ class BookmarkBaseView(BookmarkFilterBaseView):
         """Force vertical view to not use two pages."""
         rd = updates.get("reading_direction")
         if (
-            rd in VERTICAL_READING_DIRECTIONS
+            bm.two_pages
+            and rd in VERTICAL_READING_DIRECTIONS
             or bm.reading_direction in VERTICAL_READING_DIRECTIONS
-            and bm.two_pages
         ):
             updates["two_pages"] = None
 
@@ -112,16 +112,14 @@ class BookmarkBaseView(BookmarkFilterBaseView):
         for bm in existing_bookmarks:
             self._update_bookmarks_validate_page(bm, updates)
             self._update_bookmarks_validate_two_pages(bm, updates)
-            old_finished = bm.finished
             for key, value in updates.items():
                 setattr(bm, key, value)
             bm.updated_at = Now()
             update_bookmarks.append(bm)
-            finished = finished or old_finished != bm.finished
         count = len(update_bookmarks)
         if count:
             Bookmark.objects.bulk_update(update_bookmarks, tuple(update_fields))
-        return count, finished
+        return count
 
     @staticmethod
     def _create_bookmarks_validate_two_pages(updates):
@@ -135,10 +133,9 @@ class BookmarkBaseView(BookmarkFilterBaseView):
     def _create_bookmarks(self, comic_filter, search_kwargs, updates):
         """Create new bookmarks for comics that don't exist yet."""
         count = 0
-        finished = False
         self._create_bookmarks_validate_two_pages(updates)
         if not updates:
-            return count, finished
+            return count
         create_bookmarks = []
         group_acl_filter = self.get_group_acl_filter(Comic)
         exclude = {}
@@ -150,7 +147,6 @@ class BookmarkBaseView(BookmarkFilterBaseView):
             .exclude(**exclude)
             .only(*_COMIC_ONLY_FIELDS)
         )
-        finished = False
         for comic in create_bookmark_comics:
             defaults = {"comic": comic}
             defaults.update(updates)
@@ -167,19 +163,14 @@ class BookmarkBaseView(BookmarkFilterBaseView):
                 update_fields=tuple(_BOOKMARK_UPDATE_FIELDS),
                 unique_fields=Bookmark._meta.unique_together[0],  # type: ignore
             )
-            finished = updates.get("finished", False)
-        return count, finished
+        return count
 
     def update_bookmarks(self, updates, comic_filter):
         """Update a user bookmark."""
         search_kwargs = self.get_bookmark_search_kwargs(comic_filter)
-        count, finished = self._update_bookmarks(search_kwargs, updates)
-        create_count, create_finished = self._create_bookmarks(
-            comic_filter, search_kwargs, updates
-        )
-        count += create_count
-        finished = finished or create_finished
-        if count and finished:
+        count = self._update_bookmarks(search_kwargs, updates)
+        count = +self._create_bookmarks(comic_filter, search_kwargs, updates)
+        if count:
             self.notify_library_changed()
 
 
