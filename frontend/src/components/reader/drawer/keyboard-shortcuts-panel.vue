0a24ca1c44c7e0574d7169d5ad1307eb1fe09256
AJ Slater <aj@slater.net>
2024-09-11 17:59:35 -0700

    move keyboard-shortcuts content into table component. Use DrawerItem for activator.

--- frontend/src/components/reader/drawer/keyboard-shortcuts-panel.vue
+++ frontend/src/components/reader/drawer/keyboard-shortcuts-panel.vue
@@ -2,143 +2,37 @@
   <v-expansion-panels v-if="!isMobile">
     <v-expansion-panel id="readerKeyboardShortcuts">
       <v-expansion-panel-title id="shortcutsTitle">
-        <v-icon id="keyboardIcon"> {{ mdiKeyboardOutline }} </v-icon>Keyboard
-        Shortcuts
+        <DrawerItem
+          id="keyboardShortcutsItem"
+          :prepend-icon="mdiKeyboardOutline"
+          title="Keyboard Shortcuts"
+        />
       </v-expansion-panel-title>
       <v-expansion-panel-text>
-        <h4>Navigation</h4>
-        <table class="highlight-table">
-          <tbody>
-            <tr>
-              <td>
-                <v-icon class="keyIcon" density="compact">
-                  {{ mdiMenuLeft }} </v-icon
-                >, j,
-                <div>
-                  <v-icon class="keyIcon" density="compact">
-                    {{ mdiAppleKeyboardShift }}
-                  </v-icon>
-                  <v-icon class="keyIcon" density="compact">
-                    {{ mdiKeyboardSpace }}
-                  </v-icon>
-                </div>
-              </td>
-              <td>Previous page</td>
-            </tr>
-            <tr>
-              <td>
-                <v-icon class="keyIcon" density="compact">
-                  {{ mdiMenuRight }} </v-icon
-                >, k,
-                <v-icon class="keyIcon" density="compact">
-                  {{ mdiKeyboardSpace }}
-                </v-icon>
-              </td>
-              <td>Next page</td>
-            </tr>
-            <tr>
-              <td>,</td>
-              <td>Next <em>only</em> one page in two page mode.</td>
-            </tr>
-            <tr>
-              <td>.</td>
-              <td>Previous <em>only</em> one page in two page mode.</td>
-            </tr>
-            <tr>
-              <td>p</td>
-              <td>Previous Book</td>
-            </tr>
-            <tr>
-              <td>n</td>
-              <td>Next Book</td>
-            </tr>
-            <tr>
-              <td>esc</td>
-              <td>Close book</td>
-            </tr>
-          </tbody>
-        </table>
-        <h4>Fit Page To</h4>
-        <table class="highlight-table">
-          <tbody>
-            <tr>
-              <td>s</td>
-              <td>screen</td>
-            </tr>
-            <tr>
-              <td>h</td>
-              <td>screen height</td>
-            </tr>
-            <tr>
-              <td>w</td>
-              <td>screen width</td>
-            </tr>
-            <tr>
-              <td>o</td>
-              <td>Show page at original size</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Toggle two page view</td>
-            </tr>
-            <tr>
-              <td>r</td>
-              <td>Read Right to Left</td>
-            </tr>
-            <tr>
-              <td>l</td>
-              <td>Read Left to Right</td>
-            </tr>
-            <tr>
-              <td>t</td>
-              <td>Read Top to Bottom</td>
-            </tr>
-            <tr>
-              <td>b</td>
-              <td>Read in Bottom to Top</td>
-            </tr>
-          </tbody>
-        </table>
-        <h4>Other</h4>
-        <table class="highlight-table">
-          <tbody>
-            <tr>
-              <td>m</td>
-              <td>Show comic's metadata</td>
-            </tr>
-            <tr>
-              <td>double click</td>
-              <td>Zoom page</td>
-            </tr>
-          </tbody>
-        </table>
+        <KeyboardShortcutsTable />
       </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
 
 <script>
-import {
-  mdiAppleKeyboardShift,
-  mdiKeyboardOutline,
-  mdiKeyboardSpace,
-  mdiMenuLeft,
-  mdiMenuRight,
-} from "@mdi/js";
+import { mdiKeyboardOutline } from "@mdi/js";
 import { mapGetters } from "pinia";
 
+import DrawerItem from "@/components/drawer-item.vue";
+import KeyboardShortcutsTable from "@/components/reader/drawer/keyboard-shortcuts-table.vue";
 import { useCommonStore } from "@/stores/common";
 
 export default {
   // eslint-disable-next-line no-secrets/no-secrets
   name: "KeyboardShortcutsPanel",
+  components: {
+    DrawerItem,
+    KeyboardShortcutsTable,
+  },
   data() {
     return {
-      mdiMenuLeft,
-      mdiMenuRight,
       mdiKeyboardOutline,
-      mdiKeyboardSpace,
-      mdiAppleKeyboardShift,
     };
   },
   computed: {
@@ -151,43 +45,24 @@ export default {
 #readerKeyboardShortcuts {
   background-color: rgb(var(--v-theme-background));
 }
+
 #shortcutsTitle {
   padding: 0;
   padding-left: 17px;
   padding-right: 10px;
   font-size: 16px;
 }
-#keyboardIcon {
-  margin-right: 0.33em !important;
-  color: rgb(var(--v-theme-textDisabled));
+
+#keyboardShortcutsItem {
+  padding-left: 0px;
 }
+
 :deep(.v-expansion-panel-title--active) {
   min-height: 48px !important;
 }
+
 :deep(.v-expansion-panel-text__wrapper) {
   padding-left: 10px;
   padding-right: 10px;
-}
-.highlight-table {
-  margin-top: 10px;
-  border-collapse: collapse;
-  color: rgb(var(--v-theme-textDisabled));
-}
-h4 {
-  padding-left: 0.5em;
-  color: rgb(var(--v-theme-textSecondary));
-}
-.highlight-table tr:nth-child(odd) {
-  background-color: rgb(var(--v-theme-surface)) !important;
-}
-.highlight-table td {
-  min-width: 60px;
-  padding: 5px;
-}
-.highlight-table td:first-child {
-  text-align: center;
-}
-.keyIcon {
-  margin: 0;
 }
 </style>
