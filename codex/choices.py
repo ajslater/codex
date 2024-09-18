a8d1298f6c09725f810965b2f4b8a9702d737768
AJ Slater <aj@slater.net>
2024-09-17 19:59:08 -0700

    refactor choices dumper to be clearer

--- codex/choices.py
+++ codex/choices.py
@@ -229,6 +229,10 @@ WEBSOCKET_MESSAGES = MappingProxyType(
 )
 DUMMY_NULL_NAME = "_none_"
 
+################
+# Dump to JSON #
+################
+
 _DEFAULTS = MappingProxyType(
     {"browser-choices.json": BROWSER_DEFAULTS, "reader-choices.json": READER_DEFAULTS}
 )
@@ -253,44 +257,48 @@ _LOOKUP_DUMPS = MappingProxyType(
 )
 
 
-def _to_vuetify_item(value, title, is_default):
-    vuetfy_dict = {
-        "value": value,
-        "title": title,
-    }
-    if is_default:
-        vuetfy_dict["default"] = True
-    return vuetfy_dict
+def _to_vuetify_choices(defaults, key, obj_map):
+    """Transform a dict into a list of vuetify choices."""
+    default = defaults.get(key)
+    vuetify_list = []
+    for value, title in obj_map.items():
+        vuetify_dict = {
+            "value": value,
+            "title": title,
+        }
+        if default == value:
+            vuetify_dict["default"] = True
+        vuetify_list.append(vuetify_dict)
+    return vuetify_list
+
+
+def _make_json_serializable(data):
+    """Convert nested Mapping objects to dicts."""
+    if isinstance(data, Mapping):
+        return {key: _make_json_serializable(value) for key, value in data.items()}
+    if isinstance(data, list | tuple | frozenset | set):
+        return [_make_json_serializable(item) for item in data]
+    return data
 
 
 def _to_vuetify_dict(fn, data):
+    """Convert mappings to vuetify dict list."""
     vuetify_data = {}
     defaults = _DEFAULTS.get(fn) or {}
     for key, obj in data.items():
         if isinstance(obj, Mapping):
-            default = defaults.get(key)
-            vuetify_list = []
-            for value, title in obj.items():
-                vuetify_dict = _to_vuetify_item(value, title, default == value)
-                vuetify_list.append(vuetify_dict)
-            vuetify_value = vuetify_list
+            vuetify_value = _to_vuetify_choices(defaults, key, obj)
         else:
-            vuetify_value = obj
+            vuetify_value = _make_json_serializable(obj)
         vuetify_data[camelcase(key)] = vuetify_value
     return vuetify_data
 
 
-def _mapping_to_dict(data):
-    """Convert nested Mapping objects to dicts."""
-    if isinstance(data, Mapping):
-        return {key: _mapping_to_dict(value) for key, value in data.items()}
-    if isinstance(data, list | tuple | frozenset | set):
-        return [_mapping_to_dict(item) for item in data]
-    return data
-
-
 def _dump(parent_path, fn, data, vuetify=True):
-    vuetify_data = _to_vuetify_dict(fn, data) if vuetify else _mapping_to_dict(data)
+    """Dump data to json file."""
+    vuetify_data = (
+        _to_vuetify_dict(fn, data) if vuetify else _make_json_serializable(data)
+    )
     path = parent_path / Path(fn)
     with path.open("w") as json_file:
         json.dump(vuetify_data, json_file, indent=2)
