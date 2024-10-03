24d6afc01e7a79fb9d401149559f2d24f0a62f09
AJ Slater <aj@slater.net>
2024-10-03 11:51:32 -0700

    much more sophisticated and dynamic uml diagram generator script. does less when it can.

--- bin/uml.sh
+++ bin/uml.sh
@@ -1,22 +1,57 @@
 #!/bin/bash
 # Generate a big UML png diagram of codex classes
-# Improvements:
-#   - Only run pyreverse if code changes
-#   - Only running plantuml if *.plantuml changes
-#   - Download latest plantuml version
-#   - Read package dir from pyproject.toml
+# Requires tomlq & jq
 set -euo pipefail
-PACKAGE_DIR=codex
-PLANTUML_VERSION=1.2024.7
 THIS_DIR="$(dirname "$0")/.."
 cd "$THIS_DIR" || exit 1
-export PYTHONPATH="${PYTHONPATH:-}:$THIS_DIR"
-poetry run pyreverse --colorized --output plantuml "$PACKAGE_DIR"
-export PLANTUML_LIMIT_SIZE=131072
-PLANTUML_JAR=plantuml-${PLANTUML_VERSION}.jar
-if [ ! -f "$PLANTUML_JAR" ]; then
-  PLANTUML_URL=https://github.com/plantuml/plantuml/releases/download/v${PLANTUML_VERSION}/$PLANTUML_JAR
-  wget "$PLANTUML_URL"
+
+CLASSES_UML=classes.plantuml
+PACKAGES_UML=packages.plantuml
+is_uml_out_of_date() {
+  find "$2" -type f -name "*.py" | while read -r file; do
+    if [ "$1" -ot "$file" ]; then
+      echo 'out_of_date'
+    fi
+  done
+}
+
+ensure_latest_jar() {
+  # Get latest plantuml jar
+  latest_version() {
+    curl --silent "https://api.github.com/repos/$1/releases/latest" | jq -r .tag_name
+  }
+  echo "Checking plantuml for updates..."
+  local PLANTUML_VERSION
+  PLANTUML_VERSION=$(latest_version plantuml/plantuml)
+  local PLANTUML_JAR
+  PLANTUML_JAR=plantuml-${PLANTUML_VERSION:1}.jar
+  if [ ! -f "$PLANTUML_JAR" ]; then
+    local PLANTUML_URL
+    PLANTUML_URL=https://github.com/plantuml/plantuml/releases/download/${PLANTUML_VERSION}/$PLANTUML_JAR
+    curl --location --remote-name --progress-bar "$PLANTUML_URL"
+  fi
+}
+
+# Hacky sed regex gets the first include value in packages.
+PACKAGE_DIR=$(tq -f pyproject.toml tool.poetry.packages | sed -n 's/^\[{ include = "\([^"]*\)".*/\1/p')
+
+UML_OUT_OF_DATE=$(is_uml_out_of_date "$CLASSES_UML" "$PACKAGE_DIR")
+
+if [ "$UML_OUT_OF_DATE" != "" ]; then
+  export PYTHONPATH="${PYTHONPATH:-}:$THIS_DIR"
+  echo "Generating UML..."
+  poetry run pyreverse --colorized --output plantuml "$PACKAGE_DIR"
+  echo "done."
+else
+  echo "UML is up to date."
 fi
-java -jar "$PLANTUML_JAR" classes.plantuml
-java -jar "$PLANTUML_JAR" packages.plantuml
+
+if [[ $CLASSES_UML -ot classes.png ]]; then
+  echo "UML images are up to date."
+  exit 0
+fi
+ensure_latest_jar
+
+export PLANTUML_LIMIT_SIZE=65536
+java -jar "$PLANTUML_JAR" "$CLASSES_UML"
+java -jar "$PLANTUML_JAR" "$PACKAGES_UML"
