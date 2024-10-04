e3fa8587a3af0c20631469b3a1a6f86ea5e5c81c
AJ Slater <aj@slater.net>
2024-10-04 11:06:26 -0700

    change integrity check results to warning. adjust language

--- codex/librarian/janitor/integrity.py
+++ codex/librarian/janitor/integrity.py
@@ -279,21 +279,20 @@ def _is_integrity_ok(results):
 
 def integrity_check(long=True, log=None):
     """Run sqlite3 integrity check."""
-    pragma = "integrity_check" if long else "quick_check"
-    sql = _PRAGMA_TMPL % pragma
-    results = _exec_sql(sql)
-
     if not log:
         log = LOG
+    pragma = "integrity_check" if long else "quick_check"
+    sql = _PRAGMA_TMPL % pragma
+    log.debug(f"Running database '{sql}'...")
+    results = _exec_sql(sql)
 
     if _is_integrity_ok(results):
-        length = "" if long else "quick"
-        log.info(f"Database passed {length} integrity check.")
+        length = "" if long else "quick "
+        log.info(f"Database passed {length}integrity check.")
     else:
-        log.error(
-            "Database integrity compromised. See the README for database rebuild instructions."
-        )
-        log.error(results)
+        log.warning(f"Database '{sql}' returned results:")
+        log.warning(results)
+        log.warning("See the README for database rebuild instructions if the above warning looks severe.")
 
 
 def fts_rebuild(log=None):
