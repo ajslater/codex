849257af6c3b1eba1b69174c8f9a137c82e222f6
AJ Slater <aj@slater.net>
2024-11-09 15:12:40 -0800

    more stringent fix for range function

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,6 +1,8 @@
 // Utility functions
 export const range = (start, end = 0) => {
-  const size = end > 0 ? end - start : start;
+  start = Number.isInteger(start) && start >= 0 ? start : 0;
+  end = Number.isInteger(end) && end >= 0 ? end : 0;
+  size = end > start ? Math.max(end - start, 0) : start;
   let result = [...Array(size).keys()];
   if (end) {
     result = result.map((i) => i + start);
