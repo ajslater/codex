8446c910c1fa55b376b398e8157fe71b2ec76b07
AJ Slater <aj@slater.net>
2024-11-09 15:56:18 -0800

    fix lack of defintion in range function

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -2,9 +2,9 @@
 export const range = (start, end = 0) => {
   start = Number.isInteger(start) && start >= 0 ? start : 0;
   end = Number.isInteger(end) && end >= 0 ? end : 0;
-  size = end > start ? Math.max(end - start, 0) : start;
+  const size = end > start ? Math.max(end - start, 0) : start;
   let result = [...Array(size).keys()];
-  if (end) {
+  if (end > 0) {
     result = result.map((i) => i + start);
   }
   return result;
