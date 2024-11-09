bba12baa26c32566ca5b9c306391f1f0fd4c721e
AJ Slater <aj@slater.net>
2024-11-09 14:56:40 -0800

    fix negative integer is boolean true error in range

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,6 +1,6 @@
 // Utility functions
 export const range = (start, end = 0) => {
-  const size = end ? end - start : start;
+  const size = end > 0 ? end - start : start;
   let result = [...Array(size).keys()];
   if (end) {
     result = result.map((i) => i + start);
