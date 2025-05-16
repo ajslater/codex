a523f43e27c36e2327814a001dc12235ea96295a
AJ Slater <aj@slater.net>
2025-05-16 15:13:22 -0700

    fix codex.settings import path

--- bin/manage.py
+++ bin/manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run the server."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codex.settings.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codex.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
