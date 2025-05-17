f08e17875fae5c5acf957dff9a35270208273444
AJ Slater <aj@slater.net>
2025-05-16 23:50:18 -0700

    simplify max_none

--- codex/util.py
+++ codex/util.py
@@ -5,13 +5,7 @@ from collections.abc import Mapping
 
 def max_none(*args):
     """None aware math.max."""
-    max_arg = None
-    for arg in args:
-        if max_arg is None:
-            max_arg = arg
-        elif arg is not None:
-            max_arg = max(max_arg, arg)
-    return max_arg
+    return max(filter(None.__ne__, args), default=None)
 
 
 def mapping_to_dict(data):
