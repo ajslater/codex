a9ae8fd0b01140f33a4a1a7a6087d1d458a2bc62
AJ Slater <aj@slater.net>
2025-05-23 19:12:14 -0700

    shellharden --check

--- bin/lint-backend.sh
+++ bin/lint-backend.sh
@@ -28,7 +28,7 @@ if [ "$(uname)" = "Darwin" ]; then
   # Hadolint & shfmt are difficult to install on linux
   # shellcheck disable=2035
   hadolint docker/*Dockerfile
-  shellharden ./**/*.sh .env.platforms
+  shellharden --check ./**/*.sh .env.platforms
   # subdirs aren't copied into docker builder
   # .env files aren't copied into docker
   shellcheck --external-sources ./**/*.sh
