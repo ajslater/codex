2193d9c2be5c3504fb2614702b1a3ceaaf42dba2
AJ Slater <aj@slater.net>
2025-06-28 11:53:15 -0700

    remove status index prepare status

--- codex/librarian/scribe/search/status.py
+++ codex/librarian/scribe/search/status.py
@@ -7,7 +7,6 @@ class SearchIndexStatusTypes(TextChoices):
     """Search Index Status Types."""
 
     SEARCH_INDEX_CLEAR = "SIX"
-    SEARCH_INDEX_PREPARE = "SIP"
     SEARCH_INDEX_UPDATE = "SIU"
     SEARCH_INDEX_CREATE = "SIC"
     SEARCH_INDEX_REMOVE = "SID"
