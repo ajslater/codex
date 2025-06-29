9c4d1f5bd8ed5f28aa42017856120b30ce0ed728
AJ Slater <aj@slater.net>
2025-06-29 13:14:27 -0700

    move methods from create/__init__ to create/foreign_keys

--- codex/librarian/scribe/importer/create/foreign_keys.py
+++ codex/librarian/scribe/importer/create/foreign_keys.py
@@ -12,6 +12,7 @@ from codex.librarian.scribe.importer.const import (
     CREATE_FKS,
     GROUP_MODEL_COUNT_FIELDS,
     MODEL_REL_MAP,
+    TOTAL,
     UPDATE_FKS,
 )
 from codex.librarian.scribe.importer.create.const import (
@@ -26,8 +27,10 @@ from codex.librarian.scribe.importer.create.const import (
 from codex.librarian.scribe.importer.create.folders import (
     CreateForeignKeysFolderImporter,
 )
+from codex.librarian.scribe.importer.status import ImporterStatusTypes
 from codex.librarian.status import Status
 from codex.models.base import BaseModel
+from codex.models.groups import Folder
 
 
 class CreateForeignKeysCreateUpdateImporter(CreateForeignKeysFolderImporter):
@@ -185,4 +188,40 @@ class CreateForeignKeysCreateUpdateImporter(CreateForeignKeysFolderImporter):
                 model,
                 status,
             )
+        return count
+
+    def create_all_fks(self) -> int:
+        """Bulk create all foreign keys."""
+        count = 0
+        fkc = self.metadata.get(CREATE_FKS, {})
+        create_status = Status(ImporterStatusTypes.CREATE_TAGS, 0, fkc.pop(TOTAL, 0))
+        try:
+            if not fkc:
+                return count
+            self.status_controller.start(create_status)
+            count += self.bulk_folders_create(
+                fkc.pop(Folder, frozenset()), create_status
+            )
+            count += self.bulk_create_all_models(create_status)
+        finally:
+            self.metadata.pop(CREATE_FKS, None)
+            self.status_controller.finish(create_status)
+        return count
+
+    def update_all_fks(self) -> int:
+        """Bulk update all foreign keys."""
+        count = 0
+        fku = self.metadata.get(UPDATE_FKS, {})
+        update_status = Status(ImporterStatusTypes.UPDATE_TAGS, 0, fku.pop(TOTAL, 0))
+        try:
+            if not fku:
+                return count
+            self.status_controller.start(update_status)
+            count += self.bulk_folders_update(
+                fku.pop(Folder, frozenset()), update_status
+            )
+            count += self.bulk_update_all_models(update_status)
+        finally:
+            self.metadata.pop(UPDATE_FKS, None)
+            self.status_controller.finish(update_status)
         return count
