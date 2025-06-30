2a34d065301717515dd6877e94d548363e1b45b7
AJ Slater <aj@slater.net>
2025-06-30 09:59:33 -0700

    make test backend script able to run one test.

--- bin/test-backend.sh
+++ bin/test-backend.sh
@@ -2,11 +2,14 @@
 # Run codex django tests
 set -euxo pipefail
 
+TEST_LIST=("test_asgi" "test_models" "test_importer.TestImporterBasic" "test_importer.TestImporterUpdateNone" "test_importer.TestImporterUpdateAll")
 export PYTHONPATH=.
 export DJANGO_TEST_PROCESSES=auto
 # Django unit tests interfere with each other's database so cannot be run in parallel.
-uv run python3 -Wa bin/manage.py test tests.test_asgi
-uv run python3 -Wa bin/manage.py test tests.test_models
-uv run python3 -Wa bin/manage.py test tests.test_importer.TestImporterBasic
-uv run python3 -Wa bin/manage.py test tests.test_importer.TestImporterUpdateNone
-uv run python3 -Wa bin/manage.py test tests.test_importer.TestImporterUpdateAll
+if [ "${1:-}" ]; then
+  TEST_LIST=("$1")
+fi
+
+for test in "${TEST_LIST[@]}"; do
+  uv run python3 -Wa bin/manage.py test "tests.${test}"
+done
