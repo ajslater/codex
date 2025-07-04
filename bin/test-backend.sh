586fa2a8640179634e3d13890f9d524ce057f383
AJ Slater <aj@slater.net>
2025-07-04 00:02:29 -0700

    fix tests to run in parallel

--- bin/test-backend.sh
+++ bin/test-backend.sh
@@ -2,14 +2,12 @@
 # Run codex django tests
 set -euxo pipefail
 
-TEST_LIST=("test_asgi" "test_models" "test_importer.TestImporterBasic" "test_importer.TestImporterUpdateNone" "test_importer.TestImporterUpdateAll")
 export PYTHONPATH=.
-export DJANGO_TEST_PROCESSES=auto
+#export DJANGO_TEST_PROCESSES=auto
 # Django unit tests interfere with each other's database so cannot be run in parallel.
+cmd="uv run python3 -Wa bin/manage.py test --parallel auto"
 if [ "${1:-}" ]; then
-  TEST_LIST=("$1")
+    cmd="${cmd} tests.${1}"
 fi
 
-for test in "${TEST_LIST[@]}"; do
-  uv run python3 -Wa bin/manage.py test "tests.${test}"
-done
+$cmd
