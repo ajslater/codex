f5a027445af7777fa679352e72eba799223c6d3d
AJ Slater <aj@slater.net>
2025-07-09 21:01:21 -0700

    fix path

--- docker/docker-env.sh
+++ docker/docker-env.sh
@@ -2,18 +2,19 @@
 # create the docker .env firl for this architecture
 set -euo pipefail
 ./docker/install-uv.sh
+export PATH=$PATH:"$HOME/.local/bin/env"
 pip3 install --upgrade pip
 PKG_VERSION=$(./bin/version.sh)
 ENV_FN=$(./docker/docker-env-filename.sh)
 rm -f "$ENV_FN"
 # shellcheck disable=SC1091,SC2129
-cat << EOF >> "$ENV_FN"
+cat <<EOF >>"$ENV_FN"
 PKG_VERSION=${PKG_VERSION}
 CODEX_BASE_VERSION=$(./docker/docker-version-codex-base.sh)
 EOF
-echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)" >> "$ENV_FN"
-echo "CODEX_DIST_BUILDER_VERSION=$(./docker/docker-version-codex-dist-builder.sh)" >> "$ENV_FN"
-cat << EOF >> "$ENV_FN"
+echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)" >>"$ENV_FN"
+echo "CODEX_DIST_BUILDER_VERSION=$(./docker/docker-version-codex-dist-builder.sh)" >>"$ENV_FN"
+cat <<EOF >>"$ENV_FN"
 CODEX_ARCH_VERSION=$(./docker/docker-version-codex-arch.sh)
 CODEX_WHEEL=codex-${PKG_VERSION}-py3-none-any.whl
 EOF
