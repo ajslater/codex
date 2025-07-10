73e8f982c0425212290f08a55210c2668d8d799b
AJ Slater <aj@slater.net>
2025-07-10 12:13:36 -0700

    format lint

--- bin/sortignore.sh
+++ bin/sortignore.sh
@@ -2,11 +2,11 @@
 # Sort all ignore files in place and remove duplicates
 # Place ! exclusions at the end.
 for fn in .*ignore; do
-    if [ ! -L "$fn" ]; then
-        output=$(
-            grep -v '^!' "$fn" | sort --mmap --unique
-            grep '^!' "$fn" | sort --mmap --unique
-        )
-        echo "$output" >"$fn"
-    fi
+  if [ ! -L "$fn" ]; then
+    output=$(
+      grep -v '^!' "$fn" | sort --mmap --unique
+      grep '^!' "$fn" | sort --mmap --unique
+    )
+    echo "$output" > "$fn"
+  fi
 done
--- docker/docker-env.sh
+++ docker/docker-env.sh
@@ -9,13 +9,13 @@ pip3 install --upgrade pip
 ENV_FN=$(./docker/docker-env-filename.sh)
 rm -f "$ENV_FN"
 # shellcheck disable=SC1091,SC2129
-cat <<EOF >>"$ENV_FN"
+cat << EOF >> "$ENV_FN"
 PKG_VERSION=${PKG_VERSION}
 CODEX_BASE_VERSION=$(./docker/docker-version-codex-base.sh)
 EOF
-echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)" >>"$ENV_FN"
-echo "CODEX_DIST_BUILDER_VERSION=$(./docker/docker-version-codex-dist-builder.sh)" >>"$ENV_FN"
-cat <<EOF >>"$ENV_FN"
+echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)" >> "$ENV_FN"
+echo "CODEX_DIST_BUILDER_VERSION=$(./docker/docker-version-codex-dist-builder.sh)" >> "$ENV_FN"
+cat << EOF >> "$ENV_FN"
 CODEX_ARCH_VERSION=$(./docker/docker-version-codex-arch.sh)
 CODEX_WHEEL=codex-${PKG_VERSION}-py3-none-any.whl
 EOF
