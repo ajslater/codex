eb1adc8736d17da602331abc7b0246d0b8f69ddb
AJ Slater <aj@slater.net>
2025-07-12 14:32:37 -0700

    cleaner uv outdated output

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -2,7 +2,7 @@
 # Update python and npm dependencies
 set -euo pipefail
 uv sync --no-install-project --all-extras --upgrade
-uv tree --outdated
+uv tree --outdated | grep "^├.*latest" || true
 npm update
 npm outdated
 bash -c "cd frontend && bin/update-deps.sh"
