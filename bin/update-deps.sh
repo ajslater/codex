8a1428b63e20bedea17433519ec7649ab2d0ef2a
AJ Slater <aj@slater.net>
2025-10-15 13:08:00 -0700

    fix update-deps. u

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Update python and npm dependencies
 set -euo pipefail
-uv sync --no-install-project --all-extras --upgrade
-# uv tree --outdated | grep "^├.*latest" || true
-uv lock --upgrade --dry-run
+uv sync --no-install-project --all-extras --all-groups --all-packages --upgrade
+uv tree --depth 1 --outdated | grep "latest" || true
 npm update
 npm outdated
 bash -c "cd frontend && bin/update-deps.sh"
