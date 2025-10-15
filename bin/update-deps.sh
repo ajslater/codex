fab31372fa64667ba25e72c30644aa815427dc4b
AJ Slater <aj@slater.net>
2025-10-15 13:10:15 -0700

    fix color in update-deps grep

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -2,7 +2,11 @@
 # Update python and npm dependencies
 set -euo pipefail
 uv sync --no-install-project --all-extras --all-groups --all-packages --upgrade
-uv tree --depth 1 --outdated | grep "latest" || true
+uv tree --all-groups --depth 1 --upgrade --outdated | grep --color=always "(latest:.*)" || true
 npm update
 npm outdated
-bash -c "cd frontend && bin/update-deps.sh"
+if [ -d frontend ]; then
+  cd frontend
+  npm update
+  npm outdated
+fi
