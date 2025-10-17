dd845bf61e41a1a635632cbe8642f1e7c599246f
AJ Slater <aj@slater.net>
2025-10-17 12:37:49 -0700

    fix source env for running docker containers

--- docker/docker-compose-exit.sh
+++ docker/docker-compose-exit.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Run a docker compose service and return its exit code
 set -euo pipefail
-#ENV_FN=$(./docker/docker-env-filename.sh)
+ENV_FN=$(./docker/docker-env-filename.sh)
+set -a
 # shellcheck disable=SC1090
-#source "$ENV_FN"
+. "$ENV_FN"
+set +x
 #export CODEX_BUILDER_BASE_VERSION
 #export CODEX_DIST_BUILDER_VERSION
 #export CODEX_WHEEL
