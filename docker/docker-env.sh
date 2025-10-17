31cf81bbd100d7154af604ae20b046294da88ee3
AJ Slater <aj@slater.net>
2025-10-17 12:24:33 -0700

    prevent double variables in env file

--- docker/docker-env.sh
+++ docker/docker-env.sh
@@ -4,22 +4,22 @@ set -euo pipefail
 ENV_FN=$(./docker/docker-env-filename.sh)
 
 if [[ $* == *base* ]]; then
-  echo "CODEX_BASE_VERSION=$(./docker/docker-version-codex-base.sh)" > "$ENV_FN"
+  echo "CODEX_BASE_VERSION=$(./docker/docker-version-codex-base.sh)" >"$ENV_FN"
 fi
 
-if [[ $* == *builder* ]]; then
-  echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)" >> "$ENV_FN"
+if [[ $* == *builder-base* ]]; then
+  echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)" >>"$ENV_FN"
 fi
 
-if [[ $* == *dist* ]]; then
-  echo "CODEX_DIST_BUILDER_VERSION=$(./docker/docker-version-codex-dist-builder.sh)" >> "$ENV_FN"
+if [[ $* == *dist-builder* ]]; then
+  echo "CODEX_DIST_BUILDER_VERSION=$(./docker/docker-version-codex-dist-builder.sh)" >>"$ENV_FN"
 fi
 
 if [[ $* == *arch* ]]; then
-  echo "CODEX_ARCH_VERSION=$(./docker/docker-version-codex-arch.sh)" >> "$ENV_FN"
+  echo "CODEX_ARCH_VERSION=$(./docker/docker-version-codex-arch.sh)" >>"$ENV_FN"
 fi
 
 if [[ $* == *wheel* ]]; then
   PKG_VERSION=$(uv version --short)
-  echo "CODEX_WHEEL=codex-${PKG_VERSION}-py3-none-any.whl" >> "$ENV_FN"
+  echo "CODEX_WHEEL=codex-${PKG_VERSION}-py3-none-any.whl" >>"$ENV_FN"
 fi
