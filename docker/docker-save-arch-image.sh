1700cc9410d000c380cf692af187730427524c7b
AJ Slater <aj@slater.net>
2025-10-17 13:43:16 -0700

    fix unbound variable inf save arch image

--- docker/docker-save-arch-image.sh
+++ docker/docker-save-arch-image.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Save arch image
 set -xeuo pipefail
+. ./docker/machine-env.sh
 ARCH=$(./docker/docker-arch.sh)
 docker image save docker.io/ajslater/codex-arch:"$CODEX_ARCH_VERSION" -o codex-"$ARCH".tar
