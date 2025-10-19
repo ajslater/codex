4550af5b89939ac6d188f1deb1a50d85110d220c
AJ Slater <aj@slater.net>
2025-10-18 17:31:17 -0700

    debug for deploy.sh to get aournd circlici security obfuscation

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+# Load arch images and push all archs as one image to docker.io
 set -euxo pipefail
 . ./docker/machine-env.sh
 IMAGE=docker.io/ajslater/codex
 ARCH_IMAGE="docker.io/ajslater/codex-arch"
 if [[ $PKG_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  # If the version is just numbers push it as latest
   CODEX_LATEST=1
 fi
-
-#docker image pull --platform x86_64 "ajslater/codex-arch:${PKG_VERSION}-x86_64"
-#docker image pull --platform aarch64 "ajslater/codex-arch:${PKG_VERSION}-aarch64"
 
 docker image load --input codex-x86_64.tar
 docker image load --input codex-aarch64.tar
 
 TAG_ARGS=(-t "$IMAGE:$PKG_VERSION")
 if [ "${CODEX_LATEST:-}" != "" ]; then
-  # If the version is just numbers push it as latest
   TAG_ARGS+=(-t "$IMAGE":latest)
 fi
 
-docker images # debug
+# DEBUG ATTEMPT
+docker images
+docker images | awk 'NR > 1 {print $1}' | while read -r repository; do
+  echo "$repository" | sed 's/./&_/g' | sed 's/_$//'
+done
+
 docker buildx imagetools create \
   "${TAG_ARGS[@]}" \
   "${ARCH_IMAGE}:${PKG_VERSION}-x86_64" \
