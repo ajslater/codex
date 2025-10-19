8ae108827ecabbd155483958b102f86cbd0ec5be
AJ Slater <aj@slater.net>
2025-10-18 17:36:27 -0700

    lint

--- bin/roman.sh
+++ bin/roman.sh
@@ -18,7 +18,7 @@ if [ "$1" = "" ]; then
   exit 1
 fi
 
-if command -v ggrep &>/dev/null; then
+if command -v ggrep &> /dev/null; then
   GREP_CMD=$(command -v ggrep)
 else
   GREP_CMD=$(command -v grep)
@@ -34,7 +34,7 @@ good=1
 while read -ra fn; do
   # sc doesn't understand that fn isn't an array
   # shellcheck disable=2128
-  bad=$(sed -n '2 p' <"$fn" | "$GREP_CMD" -v '^# +*')
+  bad=$(sed -n '2 p' < "$fn" | "$GREP_CMD" -v '^# +*')
   if [ "$bad" ]; then
     # shellcheck disable=2128
     echo "🔪 $fn"
