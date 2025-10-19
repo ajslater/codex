b6ca86ed1746e3a3935c4c167ec2050e8f816b08
AJ Slater <aj@slater.net>
2025-10-18 23:26:26 -0700

    make docker-push use an array of arches

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -4,29 +4,25 @@ set -euxo pipefail
 . ./docker/machine-env.sh
 IMAGE=docker.io/ajslater/codex
 ARCH_IMAGE="ajslater/codex-arch"
+ARCHES=(x86_64 aarch64)
 if [[ $CODEX_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   # If the version is just numbers push it as latest
   CODEX_LATEST=1
 fi
 
-docker image load --input codex-x86_64.tar
-docker image load --input codex-aarch64.tar
-
 TAG_ARGS=(-t "$IMAGE:$CODEX_VERSION")
 if [ "${CODEX_LATEST:-}" != "" ]; then
   TAG_ARGS+=(-t "$IMAGE":latest)
 fi
-
-# DEBUG ATTEMPT
-docker images
-docker images | awk 'NR > 1 {print $1}' | while read -r repository; do
-  echo "$repository" | sed 's/./&_/g' | sed 's/_$//'
+IMAGE_ARGS=()
+for arch in "${ARCHES[@]}"; do
+  IMAGE_ARGS+=("${ARCH_IMAGE}:${CODEX_VERSION}-${arch}")
+  docker image load --input codex-"$arch".tar
 done
 
 docker buildx imagetools create \
   "${TAG_ARGS[@]}" \
-  "${ARCH_IMAGE}:${CODEX_VERSION}-x86_64" \
-  "${ARCH_IMAGE}:${CODEX_VERSION}-aarch64"
+  "${IMAGE_ARGS[@]}"
 
 docker buildx imagetools inspect "$IMAGE:$CODEX_VERSION"
 if [ "${CODEX_LATEST:-}" != "" ]; then
