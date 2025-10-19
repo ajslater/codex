0d2f528bd855bd1bd922ffded4467a4797812f9b
AJ Slater <aj@slater.net>
2025-10-18 18:31:53 -0700

    simplify .env creation

--- docker/docker-env.sh
+++ docker/docker-env.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
-# create the docker .env for this architecture
+# Create the docker .env for this architecture
 set -euo pipefail
 . ./docker/docker-env-filename.sh
 
 if [[ $* == *base* ]]; then
-  echo "CODEX_BASE_VERSION=$(./docker/docker-version-codex-base.sh)" > "$ENV_FN"
-fi
-
-if [[ $* == *builder-base* ]]; then
-  echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)" >> "$ENV_FN"
+  PKG_VERSION=$(uv version --short)
+  (
+    echo "CODEX_BASE_VERSION=$(./docker/docker-version-codex-base.sh)"
+    echo "CODEX_BUILDER_BASE_VERSION=$(./docker/docker-version-codex-builder-base.sh)"
+    echo "PKG_VERSION=${PKG_VERSION}"
+    echo "CODEX_ARCH_VERSION=$(./docker/docker-version-codex-arch.sh)"
+    echo "CODEX_WHEEL=codex-${PKG_VERSION}-py3-none-any.whl"
+  ) > "$ENV_FN"
 fi
 
 if [[ $* == *dist-builder* ]]; then
   echo "CODEX_DIST_BUILDER_VERSION=$(./docker/docker-version-codex-dist-builder.sh)" >> "$ENV_FN"
-fi
-
-if [[ $* == *arch* ]]; then
-  echo "CODEX_ARCH_VERSION=$(./docker/docker-version-codex-arch.sh)" >> "$ENV_FN"
-fi
-
-if [[ $* == *wheel* ]]; then
-  PKG_VERSION=$(uv version --short)
-  echo "PKG_VERSION=${PKG_VERSION}" >> "$ENV_FN"
-  echo "CODEX_WHEEL=codex-${PKG_VERSION}-py3-none-any.whl" >> "$ENV_FN"
 fi
