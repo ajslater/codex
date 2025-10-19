8a78f6755eb73248bb0fc860c7acae6eb71b0ddd
AJ Slater <aj@slater.net>
2025-10-19 13:36:24 -0700

    remove old scripts

--- docker/old/docker-save-arch-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Save arch image
-set -xeuo pipefail
-. ./docker/machine-env.sh
-ARCH=$(./docker/docker-arch.sh)
-docker image save docker.io/ajslater/codex-arch:"$CODEX_ARCH_VERSION" --output codex-"$ARCH".tar
