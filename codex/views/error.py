91c7ea69b459829d41d40358235d29d186e809ae
AJ Slater <aj@slater.net>
2026-01-05 17:21:11 -0800

    reset top group on exception redirect

--- codex/views/error.py
+++ codex/views/error.py
@@ -7,15 +7,20 @@ from rest_framework.views import exception_handler
 
 from codex.views.exceptions import SeeOtherRedirectError
 
-_OPDS_PREFIX = "opds/v"
+_OPDS_PATH_PREFIX = "opds/v"
+_RESET_TOP_GROUP_QUERY = "?topGroup=p"
 
 
 def codex_exception_handler(exc, context):
     """Assume OPDS clients want redirects instead of errors."""
     response = None
     request = context.get("request")
-    if _OPDS_PREFIX in request.path:
-        name = "opds:v2:feed" if _OPDS_PREFIX + "2" in request.path else "opds:v1:feed"
+    if _OPDS_PATH_PREFIX in request.path:
+        name = (
+            "opds:v2:feed"
+            if _OPDS_PATH_PREFIX + "2" in request.path
+            else "opds:v1:feed"
+        )
         if isinstance(exc, SeeOtherRedirectError):
             response = exc.get_response(name)
         elif hasattr(exc, "status_code") and exc.status_code in (
@@ -23,6 +28,7 @@ def codex_exception_handler(exc, context):
             status.HTTP_404_NOT_FOUND,
         ):
             url = reverse(name)
+            url += _RESET_TOP_GROUP_QUERY
             response = redirect(url, permanent=False)
 
     if not response:
