5b7f115ef39557ab046475e737170c073aa8374a
AJ Slater <aj@slater.net>
2026-01-06 16:25:33 -0800

    only ingore page content negotiation for opds

--- codex/views/reader/page.py
+++ codex/views/reader/page.py
@@ -6,8 +6,6 @@ from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import OpenApiParameter, extend_schema
 from loguru import logger
 from rest_framework.exceptions import NotFound
-from rest_framework.negotiation import BaseContentNegotiation
-from typing_extensions import override
 
 from codex.librarian.bookmark.tasks import BookmarkUpdateTask
 from codex.librarian.mp_queue import LIBRARIAN_QUEUE
@@ -19,34 +17,11 @@ from codex.views.bookmark import BookmarkAuthMixin
 _PDF_MIME_TYPE = "application/pdf"
 
 
-class IgnoreClientContentNegotiation(BaseContentNegotiation):
-    """Hack for clients with wild accept headers."""
-
-    @override
-    def select_parser(self, request, parsers):
-        """Select the first parser in the `.parser_classes` list."""
-        return next(iter(parsers))
-
-    @override
-    def select_renderer(
-        self,
-        request,
-        renderers,
-        format_suffix="",
-    ):
-        """Select the first renderer in the `.renderer_classes` list."""
-        renderer = next(iter(renderers))
-        return (renderer, renderer.media_type)
-
-
 class ReaderPageView(BookmarkAuthMixin, AuthFilterAPIView):
     """Display a comic page from the archive itself."""
 
     X_MOZ_PRE_HEADERS = frozenset({"prefetch", "preload", "prerender", "subresource"})
     content_type = "image/jpeg"
-    content_negotiation_class: type[BaseContentNegotiation] = (  # pyright:ignore[reportIncompatibleVariableOverride]
-        IgnoreClientContentNegotiation
-    )
 
     def _update_bookmark(self):
         """Update the bookmark if the bookmark param was passed."""
