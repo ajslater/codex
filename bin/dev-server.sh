243a4d90c2db5a657e5fa437213cfdfe229d0ce5
AJ Slater <aj@slater.net>
2026-01-23 13:16:01 -0800

    turn on pythondebug mode for dev

--- bin/dev-server.sh
+++ bin/dev-server.sh
@@ -3,10 +3,11 @@
 set -euxo pipefail
 THIS_DIR="$(dirname "$0")/.."
 cd "$THIS_DIR" || exit 1
-export PYTHONPATH="${PYTHONPATH:-}:$THIS_DIR"
 export DEBUG="${DEBUG:-1}"
+export PYTHONDEBUG=1
 export PYTHONDEVMODE="$DEBUG"
 export PYTHONDONTWRITEBYTECODE=1
+export PYTHONPATH="${PYTHONPATH:-}:$THIS_DIR"
 export PYTHONWARNINGS=always
 #export CODEX_THROTTLE_OPDS=10
 #export CODEX_THROTTLE_USER=10
