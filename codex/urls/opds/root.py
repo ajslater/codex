605acdd5beb7d0169b67a7b912099f3a7722832d
AJ Slater <aj@slater.net>
2026-01-30 12:55:22 -0800

    allow subtitles in links

--- codex/urls/opds/root.py
+++ codex/urls/opds/root.py
@@ -3,6 +3,8 @@
 from django.urls import include, path, re_path
 from django.views.generic.base import RedirectView
 
+from codex.views.opds.v2.feed import OPDS2StartView
+
 app_name = "opds"
 
 urlpatterns = (
@@ -13,6 +15,12 @@ urlpatterns = (
     ),
     path("bin/", include("codex.urls.opds.binary")),
     path("v1.2/", include("codex.urls.opds.v1")),
+    path(
+        "v2.0",
+        OPDS2StartView.as_view(),
+        {"group": "r", "pks": (0,), "page": 1},
+        name="start",
+    ),
     path("v2.0/", include("codex.urls.opds.v2")),
     re_path(r"auth.*", RedirectView.as_view(pattern_name="opds:auth:v1")),
     re_path(r"v?1[\.\d]*", RedirectView.as_view(pattern_name="opds:v1:start")),
