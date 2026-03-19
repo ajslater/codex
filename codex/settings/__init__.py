f2dae814a9f47a8ea22578cf4f883573f4613c70
AJ Slater <aj@slater.net>
2026-03-18 20:04:54 -0700

    add csp template context processor

--- codex/settings/__init__.py
+++ codex/settings/__init__.py
@@ -337,6 +337,7 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
+                "django.template.context_processors.csp",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
