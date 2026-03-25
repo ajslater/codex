b3dc0375c9f7563991809f727c527d285db733e7
AJ Slater <aj@slater.net>
2026-03-25 11:32:11 -0700

    remove cruft

--- frontend/src/components/browser/drawer/browser-settings-panel.vue
+++ frontend/src/components/browser/drawer/browser-settings-panel.vue
@@ -4,11 +4,6 @@
   <BrowserSettingsMisc />
 </template>
 <script>
-import { mdiReload } from "@mdi/js";
-import { mapState } from "pinia";
-
-import { useAuthStore } from "@/stores/auth";
-
 import BrowserSettingsCovers from "./browser-settings-covers.vue";
 import BrowserSettingsGroup from "./browser-settings-group.vue";
 import BrowserSettingsMisc from "./browser-settings-misc.vue";
@@ -19,17 +14,6 @@ export default {
     BrowserSettingsCovers,
     BrowserSettingsGroup,
     BrowserSettingsMisc,
-  },
-  data() {
-    return {
-      mdiReload,
-    };
-  },
-  computed: {
-    ...mapState(useAuthStore, ["isAuthorized"]),
-    ...mapState(useAuthStore, {
-      isUserAuthorized: (state) => Boolean(state.user),
-    }),
   },
 };
 </script>
