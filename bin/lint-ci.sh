5037f077fa342bc0b0b3334c055ba0062ba42c14
AJ Slater <aj@slater.net>
2026-04-06 15:34:17 -0700

    fix lint-ci

--- bin/lint-ci.sh
+++ bin/lint-ci.sh
@@ -5,6 +5,10 @@ set -euxo pipefail
 if [ "$(uname)" != "Darwin" ]; then
   exit 0
 fi
+
+if [ -f .github/workflows/ci.yml ]; then
+  actionlint .github/workflows/ci.yml
+fi
 if [ -f .circleci/config.yml ]; then
   circleci config validate .circleci/config.yml
 fi
