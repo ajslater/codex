9cc4811f2e8abc8d605d314732d81261dd3239b4
AJ Slater <aj@slater.net>
2026-04-16 13:50:23 -0700

    fix renamed BrowserSelectManyStore

--- frontend/src/components/browser/toolbars/select-many/browser-toolbar-select-many.vue
+++ frontend/src/components/browser/toolbars/select-many/browser-toolbar-select-many.vue
@@ -110,7 +110,7 @@ export default {
     };
   },
   computed: {
-    ...mapState(useSelectManyStore, [
+    ...mapState(useBrowserSelectManyStore, [
       "active",
       "selectedCount",
       "hasSelection",
