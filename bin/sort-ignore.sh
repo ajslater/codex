505baf3e3ac031df44acc03a939a73469f3dc819
AJ Slater <aj@slater.net>
2026-04-22 17:57:22 -0700

    fix sort-ignores to make deterministic across shells with different locales

--- bin/sort-ignore.sh
+++ bin/sort-ignore.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Sort all ignore files in place and remove duplicates
+# Set locale to make output deterministic across shells
+export LC_ALL=en_US.UTF-8
 for f in .*ignore; do
   if [ ! -L "$f" ]; then
     sort --mmap --unique --output="$f" "$f"
