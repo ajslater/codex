f7f0146c7983e86d847cb683b6f07401722b4095
AJ Slater <aj@slater.net>
2026-04-30 11:49:39 -0700

    refactor create custom covers import for simplicity

--- codex/librarian/scribe/importer/create/covers.py
+++ codex/librarian/scribe/importer/create/covers.py
@@ -2,6 +2,7 @@
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.functions.datetime import Now
+from django.db.models.query import QuerySet
 
 from codex.librarian.scribe.importer.const import (
     CLASS_CUSTOM_COVER_GROUP_MAP,
@@ -35,48 +36,81 @@ class CreateCoversImporter(CreateComicsImporter):
         except (IndexError, ObjectDoesNotExist):
             pass
 
+    def _update_custom_covers(
+        self, update_covers_qs: QuerySet, update_covers_count: int, status
+    ) -> int:
+        count = 0
+        status.total = update_covers_count
+        self.status_controller.start(status)
+
+        update_covers = []
+        for cover in update_covers_qs.only(*CUSTOM_COVER_UPDATE_FIELDS):
+            cover.updated_at = Now()
+            cover.presave()
+            update_covers.append(cover)
+
+        if update_covers:
+            CustomCover.objects.bulk_update(update_covers, CUSTOM_COVER_UPDATE_FIELDS)
+            update_cover_pks = update_covers_qs.values_list("pk", flat=True)
+            if LINK_COVER_PKS not in self.metadata:
+                self.metadata[LINK_COVER_PKS] = set()
+            self.metadata[LINK_COVER_PKS].update(update_cover_pks)
+            self.remove_covers(update_cover_pks, custom=True)
+            count = len(update_covers)
+            if status:
+                status.increment_complete(count)
+
+            link_covers_status = ImporterLinkCoversStatus(
+                0,
+                len(self.metadata[LINK_COVER_PKS]),
+            )
+            self.status_controller.update(link_covers_status, notify=False)
+        return count
+
     def update_custom_covers(self) -> int:
         """Update Custom Covers."""
         count = 0
         status = ImporterUpdateCoversStatus(0)
         try:
             update_covers_qs = self.metadata.pop(UPDATE_COVERS, None)
-            if not update_covers_qs:
-                self.status_controller.finish(status)
-                return count
             update_covers_count = update_covers_qs.count()
-            if not update_covers_count:
-                self.status_controller.finish(status)
-                return count
-            status.total = update_covers_count
-            self.status_controller.start(status)
-
-            update_covers = []
-            for cover in update_covers_qs.only(*CUSTOM_COVER_UPDATE_FIELDS):
-                cover.updated_at = Now()
-                cover.presave()
-                update_covers.append(cover)
-
-            if update_covers:
-                CustomCover.objects.bulk_update(
-                    update_covers, CUSTOM_COVER_UPDATE_FIELDS
+            if update_covers_count:
+                count = self._update_custom_covers(
+                    update_covers_qs, update_covers_count, status
                 )
-                update_cover_pks = update_covers_qs.values_list("pk", flat=True)
-                if LINK_COVER_PKS not in self.metadata:
-                    self.metadata[LINK_COVER_PKS] = set()
-                self.metadata[LINK_COVER_PKS].update(update_cover_pks)
-                self.remove_covers(update_cover_pks, custom=True)
-                count = len(update_covers)
-                if status:
-                    status.increment_complete(count)
-
-                link_covers_status = ImporterLinkCoversStatus(
-                    0,
-                    len(self.metadata[LINK_COVER_PKS]),
-                )
-                self.status_controller.update(link_covers_status, notify=False)
         finally:
             self.status_controller.finish(status)
+        return count
+
+    def _create_custom_covers(self, create_cover_paths, status) -> int:
+        count = 0
+        self.status_controller.start(status)
+
+        create_covers = []
+        for path in create_cover_paths:
+            cover = CustomCover(library=self.library, path=path)
+            cover.presave()
+            create_covers.append(cover)
+
+        if create_covers:
+            objs = CustomCover.objects.bulk_create(
+                create_covers,
+                update_conflicts=True,
+                update_fields=("path", "stat"),
+                unique_fields=CustomCover._meta.unique_together[0],
+            )
+            created_pks = frozenset(obj.pk for obj in objs)
+            if LINK_COVER_PKS not in self.metadata:
+                self.metadata[LINK_COVER_PKS] = set()
+            self.metadata[LINK_COVER_PKS].update(created_pks)
+            count = len(created_pks)
+            if status:
+                status.increment_complete(count)
+
+            link_covers_status = ImporterLinkCoversStatus(
+                0, len(self.metadata[LINK_COVER_PKS])
+            )
+            self.status_controller.update(link_covers_status, notify=False)
         return count
 
     def create_custom_covers(self) -> int:
@@ -86,38 +120,8 @@ class CreateCoversImporter(CreateComicsImporter):
         num_create_cover_paths = len(create_cover_paths)
         status = ImporterCreateCoversStatus(0, num_create_cover_paths)
         try:
-            if not num_create_cover_paths:
-                self.status_controller.finish(status)
-                return count
-
-            self.status_controller.start(status)
-
-            create_covers = []
-            for path in create_cover_paths:
-                cover = CustomCover(library=self.library, path=path)
-                cover.presave()
-                create_covers.append(cover)
-
-            if create_covers:
-                objs = CustomCover.objects.bulk_create(
-                    create_covers,
-                    update_conflicts=True,
-                    update_fields=("path", "stat"),
-                    unique_fields=CustomCover._meta.unique_together[0],
-                )
-                created_pks = frozenset(obj.pk for obj in objs)
-                if LINK_COVER_PKS not in self.metadata:
-                    self.metadata[LINK_COVER_PKS] = set()
-                self.metadata[LINK_COVER_PKS].update(created_pks)
-                count = len(created_pks)
-                if status:
-                    status.increment_complete(count)
-
-                link_covers_status = ImporterLinkCoversStatus(
-                    0, len(self.metadata[LINK_COVER_PKS])
-                )
-                self.status_controller.update(link_covers_status, notify=False)
-
+            if num_create_cover_paths:
+                count = self._create_custom_covers(create_cover_paths, status)
         finally:
             self.status_controller.finish(status)
         return count
